e6e56f8474775943b25b93f07d0e1688c808b19e
Dmitry Tsarevich <dimhotepus@gmail.com>
2022-01-01 20:11:40 +0300

    build: Use LTCG in Release builds

--- creategameprojects_debug.bat
+++ creategameprojects_debug.bat
@@ -6,4 +6,4 @@ if ERRORLEVEL 1 (
 )
 
 MSBuild.exe /m /p:Platform="x86" /p:Configuration=Release external/vpc/vpc.sln
-devtools\bin\vpc.exe /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:NO_STEAM /define:NO_ATI_COMPRESS /define:NO_NVTC /no_ceg /nofpo /hl2 +game /mksln hl2.sln
+devtools\bin\vpc.exe /2015 /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK /define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /define:NO_STEAM /define:NO_ATI_COMPRESS /define:NO_NVTC /define:LTCG /no_ceg /nofpo /hl2 +game /mksln hl2.sln
