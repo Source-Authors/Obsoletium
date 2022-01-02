be2cb6df43aa6c9bcf19ed8b49ea6c5ff6ef41cb
Dmitry Tsarevich <dimhotepus@gmail.com>
2022-01-02 20:24:53 +0300

    tier0: Optimize V_tier0_stricmp

--- tier0/tier0_strtools.cpp
+++ tier0/tier0_strtools.cpp
@@ -3,50 +3,56 @@
 #include "tier0_strtools.h"
 
 
-#define TOLOWERC( x )  (( ( x >= 'A' ) && ( x <= 'Z' ) )?( x + 32 ) : x )
+#define TOLOWERC( x )  (( ( (x) >= 'A' ) && ( (x) <= 'Z' ) )?( (x) + 32 ) : x )
+
 extern "C"
 int	V_tier0_stricmp(const char *s1, const char *s2 )
 {
+	if (s1 == s2) return 0;
+
 	uint8 const *pS1 = ( uint8 const * ) s1;
 	uint8 const *pS2 = ( uint8 const * ) s2;
 	for(;;)
 	{
-		int c1 = *( pS1++ );
-		int c2 = *( pS2++ );
-		if ( c1 == c2 )
 		{
-			if ( !c1 ) return 0;
-		}
-		else
-		{
-			if ( ! c2 )
+			const int c1 = *(pS1++), c2 = *(pS2++);
+			if (c1 == c2)
 			{
-				return c1 - c2;
+				if (!c1) return 0;
 			}
-			c1 = TOLOWERC( c1 );
-			c2 = TOLOWERC( c2 );
-			if ( c1 != c2 )
+			else
 			{
-				return c1 - c2;
+				if (!c2)
+				{
+					return c1 - c2;
+				}
+
+				const int cc1 = TOLOWERC(c1), cc2 = TOLOWERC(c2);
+				if (cc1 != cc2)
+				{
+					return cc1 - cc2;
+				}
 			}
 		}
-		c1 = *( pS1++ );
-		c2 = *( pS2++ );
-		if ( c1 == c2 )
+
 		{
-			if ( !c1 ) return 0;
-		}
-		else
-		{
-			if ( ! c2 )
+			const int c1 = *(pS1++), c2 = *(pS2++);
+			if (c1 == c2)
 			{
-				return c1 - c2;
+				if (!c1) return 0;
 			}
-			c1 = TOLOWERC( c1 );
-			c2 = TOLOWERC( c2 );
-			if ( c1 != c2 )
+			else
 			{
-				return c1 - c2;
+				if (!c2)
+				{
+					return c1 - c2;
+				}
+
+				const int cc1 = TOLOWERC(c1), cc2 = TOLOWERC(c2);
+				if (cc1 != cc2)
+				{
+					return cc1 - cc2;
+				}
 			}
 		}
 	}
