c87fbe12ec283e8c4e9cb6815ce586cda81f1d22
Dmitry Tsarevich <dimhotepus@gmail.com>
2022-02-21 00:12:22 +0300

    materialsystem: Restore correct texture format

--- materialsystem/ctexture.cpp
+++ materialsystem/ctexture.cpp
@@ -3311,11 +3311,11 @@ IVTFTexture *CTexture::LoadTextureBitsFromFile( char *pCacheFileName, char **ppR
 	if ( !IsProcedural() )
 		SetupDebuggingTextures( pVTFTexture );
 
-	//if ( ConvertToActualFormat( pVTFTexture ) )
-	//{
+	if ( ConvertToActualFormat( pVTFTexture ) )
+	{
 		// dimhotepus: Notify artists about bad texture format.
-	//	Warning( "\"%s\" not in final format, this is causing stutters or load time bloat!\n", pCacheFileName );
-	//}
+		Warning( "\"%s\" not in final format (%d), converting causes stutters or load time bloat!\n", pCacheFileName, m_ImageFormat );
+	}
 
 	return pVTFTexture;
 }
