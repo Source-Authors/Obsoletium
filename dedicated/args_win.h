df1545c2448de34084ec2d06f8dd5b452fa6bdf3
Dmitry Tsarevich <dimhotepus@gmail.com>
2023-12-28 13:17:43 +0300

    [dedicated] Split args_win header to remove windows headers from header

--- dedicated/args_win.h
+++ dedicated/args_win.h
@@ -6,80 +6,8 @@
 //
 //=============================================================================//
 
-#include "winlite.h"
-
-#include <shellapi.h>
-#include <memory>
 #include <system_error>
 #include <variant>
-
-#include "tier0/dbg.h"
-
-// Wide args wrapper.
-struct WideArgs
-{
-	// Construct wide args from command line. |command_line| Command line.
-	explicit WideArgs(_In_z_ const wchar_t *command_line) noexcept
-			: argv{::CommandLineToArgvW( command_line, &argc )}
-	{
-	}
-
-	~WideArgs() noexcept
-	{
-		[[maybe_unused]] const bool is_succeeded{::LocalFree( argv ) == nullptr};
-		AssertMsg(is_succeeded, "Failed to LocalFree memory from CommandLineToArgvW.");
-	}
-
-	// Argv.
-	wchar_t **argv;
-	// Argc.
-	int argc;
-};
-
-/**
- * @brief Converts wchar_t -> UTF-8.
- * @param wide wchar_t source.
- * @param out_size Out UTF-8 size.
- * @return UTF-8 string.
- */
-[[nodiscard]] std::variant<std::unique_ptr<char[]>, std::error_code> WideToUtf8(
-		_In_z_ const wchar_t *wide, size_t &out_size) noexcept 
-{
-	out_size = 0;
-
-	// Compute the size of the required buffer.
-	const int size{::WideCharToMultiByte( CP_UTF8, 0, wide, -1, nullptr, 0,
-																			  nullptr, nullptr )};
-	if ( size <= 0 ) [[unlikely]]
-	{
-		// This should never happen.
-		const auto rc = std::error_code{ (int)::GetLastError(), std::system_category() };
-		Warning( "Could not find required size for command line arguments as utf8." );
-		return rc;
-	}
-
-	// Do the actual conversion.
-	std::unique_ptr<char[]> argv{std::make_unique<char[]>( static_cast<size_t>(size) )};
-	if ( !argv ) [[unlikely]]
-	{
-		const auto rc = std::error_code{ (int)ERROR_OUTOFMEMORY, std::system_category() };
-		Warning( "Could not allocate memory to convert wchar_t {argc, argv} to UTF-8." );
-		return rc;
-	}
-
-	const int result{::WideCharToMultiByte( CP_UTF8, 0, wide, -1, argv.get(), size,
-																				  nullptr, nullptr )};
-	if ( result <= 0 ) [[unlikely]] {
-		// This should never happen.
-		const auto rc = std::error_code{ (int)::GetLastError(), std::system_category() };
-		Warning( "Could not convert command line arguments to utf8." );
-		return rc;
-	}
-
-	out_size = static_cast<size_t>(size);
-
-	return std::move(argv);
-}
 
 // Command line arguments.
 class Args
@@ -87,48 +15,7 @@ class Args
 public:
 	// Parse command line and build command line arguments pack. |command_line| Command line.
 	[[nodiscard]] static std::variant<Args, std::error_code> FromCommandLine(
-			_In_z_ const wchar_t *command_line) noexcept
-	{
-		// Parse command line to wide {argc, argv}.
-		WideArgs wargs{command_line};
-		wchar_t **wargv{wargs.argv};
-		const int argc{wargs.argc};
-
-		if ( !wargv || argc <= 0 ) [[unlikely]]
-		{
-			const auto rc = std::error_code{ (int)::GetLastError() , std::system_category() };
-			Warning( "Could not parse command line to {argc, argv} tuple." );
-			return rc;
-		}
-
-		// Convert argv to UTF-8.
-		std::unique_ptr<char *[]> argv{std::make_unique<char *[]>( static_cast<size_t>(argc) + 1) };
-		if ( !argv ) [[unlikely]]
-		{
-			const auto rc = std::error_code{ ERROR_OUTOFMEMORY, std::system_category() };
-			Warning( "Could not allocate memory to convert wchar_t {argc, argv} to UTF-8." );
-			return rc;
-		}
-
-		for (size_t i{0}; i < static_cast<size_t>(argc); i++) {
-			size_t out_size{0};
-
-			auto utf8_result = WideToUtf8( wargv[i], out_size );
-			if ( auto *arg = std::get_if<std::unique_ptr<char[]>>( &utf8_result )) [[likely]]
-			{
-				argv[i] = arg->release();
-			}
-			else
-			{
-				// Can leak argv[0..i-1], but we stop the app anyway so OS reclaims memory.
-				return std::get<std::error_code>( utf8_result );
-			}
-		}
-
-		argv[static_cast<size_t>(argc)] = nullptr;
-
-		return Args{argv.release(), argc};
-	}
+			_In_z_ const wchar_t *command_line) noexcept;
 
 	Args( Args &&args ) noexcept
 			: values_{args.values_}, count_{args.count_} {
