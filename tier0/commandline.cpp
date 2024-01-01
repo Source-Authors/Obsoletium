67a8926dd9fd269bd23f3cab24108e972036a4b3
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-01-01 21:17:51 +0300

    [tier0] Fix signed / unsigned mismatch in command line

--- tier0/commandline.cpp
+++ tier0/commandline.cpp
@@ -157,7 +157,7 @@ void CCommandLine::LoadParametersFromFile( const char *&pSrc, char *&pDst, size_
 			*pDst++ = static_cast<char>( c );
 			
 			// Don't go past the end, and allow for our terminating space character AND a terminating null character.
-			if ( (pDst - pDestStart) >= (maxDestLen-2) )
+			if ( (pDst - pDestStart) >= ((ptrdiff_t)maxDestLen-2) )
 				break;
 
 			// Get the next character, if there are more
