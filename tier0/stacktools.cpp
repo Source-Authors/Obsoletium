4544798c027280018870eee2a4644ace97b55168
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-01-02 23:31:52 +0300

    [tier0] Drop unicode char from stacktools sources

--- tier0/stacktools.cpp
+++ tier0/stacktools.cpp
@@ -1,4 +1,4 @@
-// Copyright © 1996-2005, Valve Corporation, All rights reserved.
+// Copyright 1996-2005, Valve Corporation, All rights reserved.
 
 #include "pch_tier0.h"
 #include "tier0/stacktools.h"
