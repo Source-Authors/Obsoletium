8b573448c1cb3c79816d4b48ff8842694af75962
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-03-02 01:41:45 +0300

    [utils] Add pfm2tgas utility

--- utils/pfm2tgas/pfm2tgas.cpp
+++ utils/pfm2tgas/pfm2tgas.cpp
@@ -1,91 +1,111 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+// Copyright Valve Corporation, All rights reserved.
 //
-// Purpose: 
-//
-//===========================================================================//
+// PFM to TGAs converter.
 
-#include <tier0/platform.h>
 #include "bitmap/float_bm.h"
 #include "mathlib/mathlib.h"
-#include <tier2/tier2.h>
 
-#define N_TEXTURES_OUTPUT 3
-#define TEXTURE_TO_TEXTURE_SCALE 8.0
-#define BASE_SCALE 0.25
+#include "tier0/platform.h"
+#include "tier2/tier2.h"
 
-static float Truncate8( float f )
-{
-	uint8 px= (uint8) min(255.0, (f*255.0));
-	return px*(1.0/255.0);
+#include "tools_minidump.h"
+
+namespace {
+
+[[nodiscard]] constexpr inline float Normalize2Byte(float f) {
+  const uint8 px = static_cast<uint8>(std::min(255.0f, f * 255.0f));
+  return px * (1.0f / 255.0f);
 }
 
-static float ErrorWeights[]={ 0.3, 0.59, 0.11 };
+}  // namespace
 
-void main(int argc,char **argv)
-{
-	InitCommandLineProgram( argc, argv );
-	if ( argc==3)
-	{
-		FloatBitMap_t src_texture;
-		src_texture.LoadFromPFM( argv[1] );
-		float sfactor=atof(argv[2]);
-		// we will split the float texture into 2 textures.
-		FloatBitMap_t output_textures[N_TEXTURES_OUTPUT];
-		for(int o=0; o<N_TEXTURES_OUTPUT; o++)
-		{
-			output_textures[o].AllocateRGB( src_texture.Width, src_texture.Height );
-		}
-		for(int y=0; y < src_texture.Height; y++)
-			for(int x=0; x< src_texture.Width; x++)
-			{
-				for(int c=0;c<3;c++)
-					src_texture.Pixel(x,y,c) *= sfactor;
-				float curscale=BASE_SCALE;
-				float px_error[N_TEXTURES_OUTPUT];
-				float sum_error=0;
-				for(int o=0; o<N_TEXTURES_OUTPUT; o++)
-				{
-					px_error[o]=0.;
-					for(int c=0;c<3;c++)
-					{
-						float opixel=Truncate8( src_texture.Pixel( x, y, c )/curscale );
+int main(int argc, char **argv) {
+  // Install an exception handler.
+  const se::utils::common::ScopedDefaultMinidumpHandler
+      scoped_default_minidumps;
 
-						output_textures[o].Pixel( x, y, c )= opixel;
-						float expanded_pixel = curscale * opixel;
-						float err= fabs( src_texture.Pixel( x, y, c )- expanded_pixel );
-						if (src_texture.Pixel(x,y,c) > curscale)
-							err *=2;
-						px_error[o] += ErrorWeights[o] * err;
-					}
-					sum_error += px_error[o];
-					curscale *= TEXTURE_TO_TEXTURE_SCALE;
-				}
-				// now, set the weight for each map based upon closeness of fit between the scales
-				for(int o=0; o<N_TEXTURES_OUTPUT; o++)
-				{
-					if (sum_error == 0)						// possible, I guess, for black
-					{
-						if ( o == 0)
-							src_texture.Pixel(x, y, 3) = 1.0;
-						else
-							src_texture.Pixel(x, y, 3) = 0.0;
-					}
-					else
-						output_textures[o].Pixel(x, y, 3)=1.0-(px_error[o]/sum_error);
-				}
-			}
-		// now, output results
-		for(int o=0;o<N_TEXTURES_OUTPUT;o++)
-		{
-			char fnamebuf[1024];
-			strcpy(fnamebuf, argv[1]);
-			char *dot=strchr(fnamebuf,'.');
-			if (! dot)
-				dot=fnamebuf+strlen(fnamebuf);
-			sprintf(dot,"_%d.tga",o);
-			output_textures[o].WriteTGAFile(fnamebuf);
-		}
-	}
-	else
-		printf("format is 'pfm2tgas xxx.pfm scalefactor'\n");
+  constexpr size_t kOutTexturesCount{3};
+  constexpr float kTexture2TextureScale{8.0f};
+  constexpr float kBaseScale{0.25f};
+
+  constexpr float kErrorWeights[]{0.3f, 0.59f, 0.11f};
+
+  const ScopedCommandLineProgram scoped_command_line_program(argc, argv);
+
+  if (argc != 3) {
+    fprintf(stderr, "Usage: pfm2tgas xxx.pfm scalefactor'\n");
+    return 2;
+  }
+
+  char *end{nullptr};
+  const float scale_factor{strtof(argv[2], &end)};
+  if (*end != '\0') {
+    fprintf(stderr, "Usage: pfm2tgas xxx.pfm <float scalefactor>'\n");
+    return 3;
+  }
+
+  FloatBitMap_t src_texture;
+  src_texture.LoadFromPFM(argv[1]);
+
+  // we will split the float texture into 2 textures.
+  FloatBitMap_t out_textures[kOutTexturesCount];
+  for (auto &out : out_textures) {
+    out.AllocateRGB(src_texture.Width, src_texture.Height);
+  }
+
+  for (int y = 0; y < src_texture.Height; y++)
+    for (int x = 0; x < src_texture.Width; x++) {
+      for (int c = 0; c < 3; c++) {
+        src_texture.Pixel(x, y, c) *= scale_factor;
+      }
+
+      float current_scale{kBaseScale};
+      float px_error[kOutTexturesCount];
+      float sum_error{0};
+
+      for (intp o = 0; o < ssize(px_error); o++) {
+        px_error[o] = 0.f;
+
+        for (int c = 0; c < 3; c++) {
+          const float pixel{
+              Normalize2Byte(src_texture.Pixel(x, y, c) / current_scale)};
+
+          out_textures[o].Pixel(x, y, c) = pixel;
+
+          const float scaled_pixel{current_scale * pixel};
+          float error{fabs(src_texture.Pixel(x, y, c) - scaled_pixel)};
+
+          if (src_texture.Pixel(x, y, c) > current_scale) error *= 2;
+
+          px_error[o] += kErrorWeights[o] * error;
+        }
+
+        sum_error += px_error[o];
+        current_scale *= kTexture2TextureScale;
+      }
+
+      // now, set the weight for each map based upon closeness of fit between
+      // the scales
+      for (intp o = 0; o < ssize(out_textures); o++) {
+        // possible, I guess, for black
+        if (sum_error == 0) {
+          src_texture.Pixel(x, y, 3) = o == 0 ? 1.0f : 0.0f;
+        } else {
+          out_textures[o].Pixel(x, y, 3) = 1.0f - (px_error[o] / sum_error);
+        }
+      }
+    }
+
+  char name[MAX_PATH];
+  // now, output results
+  for (int o = 0; o < kOutTexturesCount; o++) {
+    V_strcpy_safe(name, argv[1]);
+
+    char *dot = strchr(name, '.');
+    if (!dot) dot = name + strlen(name);
+
+    sprintf(dot, "_%d.tga", o);
+
+    out_textures[o].WriteTGAFile(name);
+  }
 }
