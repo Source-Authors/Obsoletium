dab9bd4fb350419aab416c2a764f19853d28102a
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-12-08 01:54:55 +0300

    [vrad_launcher] Modernize and cleanup vrad executable

--- utils/vrad_launcher/vrad_launcher.cpp
+++ utils/vrad_launcher/vrad_launcher.cpp
@@ -1,145 +1,154 @@
-//========= Copyright Valve Corporation, All rights reserved. ============//
+// Copyright Valve Corporation, All rights reserved.
 //
-// Purpose: 
-//
-// $NoKeywords: $
-//
-//=============================================================================//
-// vrad_launcher.cpp : Defines the entry point for the console application.
-//
+// Valve Radiosity launcher.
 
 #include "stdafx.h"
-#include <direct.h>
-#include "tier1/strtools.h"
+
+#include <filesystem>
+#include <system_error>
+
+#include "posix_file_stream.h"
+#include "scoped_dll.h"
+
 #include "tier0/icommandline.h"
+#include "tier1/strtools.h"
 
+#include "ivraddll.h"
 
-char* GetLastErrorString()
-{
-	static char err[2048];
-	
-	LPVOID lpMsgBuf;
-	FormatMessage( 
-		FORMAT_MESSAGE_ALLOCATE_BUFFER | 
-		FORMAT_MESSAGE_FROM_SYSTEM | 
-		FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL,
-		GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
-		(LPTSTR) &lpMsgBuf,
-		0,
-		NULL 
-	);
+namespace {
 
-	strncpy( err, (char*)lpMsgBuf, sizeof( err ) );
-	LocalFree( lpMsgBuf );
+template <int size>
+[[nodiscard]] std::error_code MakeFullPath(const char *in, char (&out)[size]) {
+  if (in[0] == CORRECT_PATH_SEPARATOR || in[0] == INCORRECT_PATH_SEPARATOR ||
+      in[1] == ':') {
+    // It's already a full path.
+    V_strncpy(out, in, size);
+  } else {
+    try {
+      std::error_code rc;
+      std::filesystem::path cwd{std::filesystem::current_path(rc)};
 
-	err[ sizeof( err ) - 1 ] = 0;
+      if (rc) return rc;
 
-	return err;
+      Q_UTF16ToUTF8(cwd.c_str(), out, size * sizeof(out[0]));
+    } catch (const std::bad_alloc &) {
+      return std::error_code{ENOMEM, std::generic_category()};
+    }
+
+    V_strncat(out, CORRECT_PATH_SEPARATOR_S, size, COPY_ALL_CHARACTERS);
+    V_strncat(out, in, size, COPY_ALL_CHARACTERS);
+  }
+
+  return std::error_code{};
 }
 
-
-void MakeFullPath( const char *pIn, char *pOut, int outLen )
-{
-	if ( pIn[0] == '/' || pIn[0] == '\\' || pIn[1] == ':' )
-	{
-		// It's already a full path.
-		Q_strncpy( pOut, pIn, outLen );
-	}
-	else
-	{
-		_getcwd( pOut, outLen );
-		Q_strncat( pOut, "\\", outLen, COPY_ALL_CHARACTERS );
-		Q_strncat( pOut, pIn, outLen, COPY_ALL_CHARACTERS );
-	}
+[[nodiscard]] int GetBothArgIndex(int argc, char *argv[]) {
+  int both_arg{0};
+  for (int arg{1}; arg < argc; arg++) {
+    if (V_stricmp(argv[arg], "-both") == 0) {
+      both_arg = arg;
+    }
+  }
+  return both_arg;
 }
 
-int main(int argc, char* argv[])
-{
-	char dllName[512];
+}  // namespace
 
-	CommandLine()->CreateCmdLine( argc, argv );
+int main(int argc, char *argv[]) {
+  // Need command line first.
+  CommandLine()->CreateCmdLine(argc, argv);
 
-	// check whether they used the -both switch. If this is specified, vrad will be run
-	// twice, once with -hdr and once without
-	int both_arg=0;
-	for(int arg=1;arg<argc;arg++)
-		if (Q_stricmp(argv[arg],"-both")==0)
-		{
-			both_arg=arg;
-		}
+  // Check whether they used the -both switch.  If this is specified, vrad will
+  // be run twice, once with -hdr and once without.
+  const int both_arg{GetBothArgIndex(argc, argv)};
 
-	char fullPath[512], redirectFilename[512];
-	MakeFullPath( argv[0], fullPath, sizeof( fullPath ) );
-	Q_StripFilename( fullPath );
-	Q_snprintf( redirectFilename, sizeof( redirectFilename ), "%s\\%s", fullPath, "vrad.redirect" );
+  constexpr char kRedirectFileName[]{"vrad.redirect"};
+  constexpr char kModuleName[]{"vrad_dll" DLL_EXT_STRING};
 
-	// First, look for vrad.redirect and load the dll specified in there if possible.
-	CSysModule *pModule = NULL;
-	FILE *fp = fopen( redirectFilename, "rt" );
-	if ( fp )
-	{
-		if ( fgets( dllName, sizeof( dllName ), fp ) )
-		{
-			char *pEnd = strstr( dllName, "\n" );
-			if ( pEnd )
-				*pEnd = 0;
+  char full_path[MAX_FILEPATH], redirect_path[MAX_FILEPATH];
+  std::error_code err{MakeFullPath(argv[0], full_path)};
+  if (err) {
+    fprintf(stderr, "vrad err: Can't get full path to '%s'.\n", argv[0]);
+    return err.value();
+  }
 
-			pModule = Sys_LoadModule( dllName );
-			if ( pModule )
-				printf( "Loaded alternate VRAD DLL (%s) specified in vrad.redirect.\n", dllName );
-			else
-				printf( "Can't find '%s' specified in vrad.redirect.\n", dllName );
-		}
-		
-		fclose( fp );
-	}
+  V_StripFilename(full_path);
+  V_sprintf_safe(redirect_path, "%s" CORRECT_PATH_SEPARATOR_S "%s", full_path,
+                 kRedirectFileName);
 
-	int returnValue = 0;
-	
-	for(int mode=0;mode<2;mode++)
-	{
-		if (mode && (! both_arg))
-			continue;
-		
+  source::ScopedDll module{nullptr, 0};
+  char module_name[MAX_FILEPATH];
 
-		// If it didn't load the module above, then use the 
-		if ( !pModule )
-		{
-			strcpy( dllName, "vrad_dll.dll" );
-			pModule = Sys_LoadModule( dllName );
-		}
-		
-		if( !pModule )
-		{
-			printf( "vrad_launcher error: can't load %s\n%s", dllName, GetLastErrorString() );
-			return 1;
-		}
-		
-		CreateInterfaceFn fn = Sys_GetFactory( pModule );
-		if( !fn )
-		{
-			printf( "vrad_launcher error: can't get factory from vrad_dll.dll\n" );
-			Sys_UnloadModule( pModule );
-			return 2;
-		}
-		
-		int retCode = 0;
-		IVRadDLL *pDLL = (IVRadDLL*)fn( VRAD_INTERFACE_VERSION, &retCode );
-		if( !pDLL )
-		{
-			printf( "vrad_launcher error: can't get IVRadDLL interface from vrad_dll.dll\n" );
-			Sys_UnloadModule( pModule );
-			return 3;
-		}
-		
-		if (both_arg)
-			strcpy(argv[both_arg],(mode)?"-hdr":"-ldr");
-		returnValue = pDLL->main( argc, argv );
-		Sys_UnloadModule( pModule );
-		pModule=0;
-	}
-	return returnValue;
+  // First, look for vrad.redirect and load the dll specified in there if
+  // possible.
+  if (auto [f, err2] =
+          se::posix::posix_file_stream_factory::open(redirect_path, "rt");
+      !err2) {
+    char *name{nullptr};
+    if (std::tie(name, err2) = f.gets(module_name); !err2) {
+      char *end{strchr(name, '\n')};
+      if (end) *end = '\0';
+
+      module = source::ScopedDll{name, 0};
+      if (!module) {
+        fprintf(stderr, "vrad warn: Can't find '%s' specified in '%s'.\n", name,
+                kRedirectFileName);
+        return module.error_code().value();
+      } else {
+        printf("vrad: Loaded alternate vrad module '%s' specified in '%s'.\n",
+               name, kRedirectFileName);
+      }
+    }
+  }
+
+  int rc{0};
+
+  // Try ldr and hdr.
+  for (int mode = 0; mode < 2; mode++) {
+    if (mode && !both_arg) continue;
+
+    // If it didn't load the module above, then use the
+    if (!module) {
+      V_strcpy_safe(module_name, kModuleName);
+
+      module = source::ScopedDll{module_name, 0};
+    }
+
+    if (!module) {
+      fprintf(stderr, "vrad error: Can't load '%s'.\n%s", module_name,
+              std::system_category().message(::GetLastError()).c_str());
+      return ENOENT;
+    }
+
+    const auto kFactoryName = CREATEINTERFACE_PROCNAME;
+    CreateInterfaceFnT<IVRadDLL> factory{nullptr};
+
+    std::tie(factory, err) =
+        module.GetFunction<CreateInterfaceFnT<IVRadDLL>>(kFactoryName);
+    if (!factory) {
+      fprintf(stderr, "vrad error: Can't get '%s' factory from '%s'.\n",
+              kFactoryName, kModuleName);
+      return EINVAL;
+    }
+
+    int ret{0};
+    const auto kInterfaceName = VRAD_INTERFACE_VERSION;
+
+    IVRadDLL *vrad{factory(kInterfaceName, &ret)};
+    if (!vrad) {
+      fprintf(stderr, "vrad error: Can't get '%s' interface from '%s'.\n",
+              kModuleName, kInterfaceName);
+      return EINVAL;
+    }
+
+    // Replace -both with -hdr or -ldr.
+    if (both_arg) V_strcpy(argv[both_arg], mode ? "-hdr" : "-ldr");
+
+    rc = vrad->main(argc, argv);
+
+    // VRAD failed, return first failure code.
+    if (rc) return rc;
+  }
+
+  return rc;
 }
-
