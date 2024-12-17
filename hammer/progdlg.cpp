d071bc70c5381d452c42ab72547d508af00b6b94
Dmitry Tsarevich <dimhotepus@gmail.com>
2024-12-17 15:12:37 +0300

    [hammer] Progress Dialog header is self-contained

--- hammer/progdlg.cpp
+++ hammer/progdlg.cpp
@@ -9,7 +9,6 @@
 // CG: This file was added by the Progress Dialog component
 
 #include "stdafx.h"
-#include "resource.h"
 #include "ProgDlg.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
@@ -96,7 +95,7 @@ void CProgressDlg::OnCancel()
     m_bCancel=TRUE;
 }
 
-void CProgressDlg::SetRange(int nLower,int nUpper)
+void CProgressDlg::SetRange(short nLower,short nUpper)
 {
     m_nLower = nLower;
     m_nUpper = nUpper;
@@ -171,24 +170,23 @@ BOOL CProgressDlg::CheckCancelButton()
 void CProgressDlg::UpdatePercent(int nNewPos)
 {
     CWnd *pWndPercent = GetDlgItem(CG_IDC_PROGDLG_PERCENT);
-    int nPercent;
     
-    int nDivisor = m_nUpper - m_nLower;
+    short nDivisor = m_nUpper - m_nLower;
     Assert(nDivisor>0);  // m_nLower should be smaller than m_nUpper
 
     int nDividend = (nNewPos - m_nLower);
     Assert(nDividend>=0);   // Current position should be greater than m_nLower
 
-    nPercent = nDividend * 100 / nDivisor;
+    int nPercent = nDividend * 100 / nDivisor;
 
     // Since the Progress Control wraps, we will wrap the percentage
     // along with it. However, don't reset 100% back to 0%
-    if(nPercent!=100)
+    if (nPercent != 100)
       nPercent %= 100;
 
     // Display the percentage
     CString strBuf;
-    strBuf.Format(_T("%d%c"),nPercent,_T('%'));
+    strBuf.Format(_T("%hd%c"),nPercent,_T('%'));
 
 	CString strCur; // get current percentage
     pWndPercent->GetWindowText(strCur);
--- hammer/progdlg.h
+++ hammer/progdlg.h
@@ -14,7 +14,9 @@
 #ifndef __PROGDLG_H__
 #define __PROGDLG_H__
 
-class CProgressDlg : public CDialog
+#include "resource.h"
+
+class CProgressDlg : public CBaseDlg
 {
 // Construction / Destruction
 public:
@@ -26,7 +28,7 @@ public:
     // Checking for Cancel button
     BOOL CheckCancelButton();
     // Progress Dialog manipulation
-    void SetRange(int nLower,int nUpper);
+    void SetRange(short nLower,short nUpper);
     int  SetStep(int nStep);
     int  SetPos(int nPos);
     int  OffsetPos(int nPos);
@@ -50,8 +52,9 @@ public:
 // Implementation
 protected:
 	UINT m_nCaptionID;
-    int m_nLower;
-    int m_nUpper;
+    // dimhotepus: int -> short as Window control expects.
+    short m_nLower;
+    short m_nUpper;
     int m_nStep;
     
     BOOL m_bCancel;
