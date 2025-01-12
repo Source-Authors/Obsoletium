e3150ac67332649d6b5aa68fc3d8f2cb7a3a6150
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-01-12 23:04:48 +0300

    [tier1] Drop x360 code from some UTL collections

--- public/tier1/utlbuffer.h
+++ public/tier1/utlbuffer.h
@@ -658,29 +658,19 @@ inline void CUtlBuffer::GetTypeBin< float >( float &dest )
 	if ( CheckGet( sizeof( float ) ) )
 	{
 		uintptr_t pData = (uintptr_t)PeekGet();
-		if ( IsX360() && ( pData & 0x03 ) )
-		{
-			// handle unaligned read
-			((unsigned char*)&dest)[0] = ((unsigned char*)pData)[0];
-			((unsigned char*)&dest)[1] = ((unsigned char*)pData)[1];
-			((unsigned char*)&dest)[2] = ((unsigned char*)pData)[2];
-			((unsigned char*)&dest)[3] = ((unsigned char*)pData)[3];
-		}
-		else
-		{
-			// aligned read
-			dest = *(float *)pData;
-		}
+		// aligned read
+		dest = *(float *)pData;
+
 		if ( m_Byteswap.IsSwappingBytes() )
 		{
 			m_Byteswap.SwapBufferToTargetEndian< float >( &dest, &dest );
 		}
-		m_Get += sizeof( float );	
-	}		
+		m_Get += sizeof( float );
+	}
 	else
 	{
 		dest = 0;
-	}					
+	}
 }
 
 template <typename T> 
