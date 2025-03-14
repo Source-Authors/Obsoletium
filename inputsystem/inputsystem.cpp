2f3c4839c2751639b3ddb27ba100b73ab05fd64f
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-03-14 14:50:48 +0300

    [inputsystem] Mark PVS-Studio warnings *WindowLongPtr as false positives

--- inputsystem/inputsystem.cpp
+++ inputsystem/inputsystem.cpp
@@ -276,8 +276,8 @@ void CInputSystem::AttachToWindow( void* hWnd )
 	}
 
 #if defined( PLATFORM_WINDOWS )
-	m_ChainedWndProc = (WNDPROC)GetWindowLongPtrW( (HWND)hWnd, GWLP_WNDPROC );
-	SetWindowLongPtrW( (HWND)hWnd, GWLP_WNDPROC, (LONG_PTR)InputSystemWindowProc );
+	m_ChainedWndProc = (WNDPROC)GetWindowLongPtrW( (HWND)hWnd, GWLP_WNDPROC ); //-V204
+	SetWindowLongPtrW( (HWND)hWnd, GWLP_WNDPROC, (LONG_PTR)InputSystemWindowProc ); //-V221
 #endif
 
 	m_hAttachedHWnd = (HWND)hWnd;
@@ -328,7 +328,7 @@ void CInputSystem::DetachFromWindow( )
 #if defined( PLATFORM_WINDOWS )
 	if ( m_ChainedWndProc )
 	{
-		SetWindowLongPtrW( m_hAttachedHWnd, GWLP_WNDPROC, (LONG_PTR)m_ChainedWndProc );
+		SetWindowLongPtrW( m_hAttachedHWnd, GWLP_WNDPROC, (LONG_PTR)m_ChainedWndProc ); //-V221
 		m_ChainedWndProc = nullptr;
 	}
 #endif
@@ -505,7 +505,7 @@ void CInputSystem::ProcessEvent( UINT uMsg, WPARAM wParam, LPARAM lParam )
 	// To prevent subtle input timing bugs, all button events must be fed 
 	// through the window proc once per frame, same as the keyboard and mouse.
 	HWND hWnd = GetFocus();
-	WNDPROC windowProc = (WNDPROC)GetWindowLongPtrW(hWnd, GWLP_WNDPROC );
+	WNDPROC windowProc = (WNDPROC)GetWindowLongPtrW(hWnd, GWLP_WNDPROC ); //-V204
 	if ( windowProc )
 	{
 		windowProc( hWnd, uMsg, wParam, lParam );
