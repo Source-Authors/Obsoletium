7a531f22d4785a5ad426b557f02e8e45b0f2bfdd
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-03-14 05:40:44 +0300

    [tier1] Fix comparison API for CAlignedMemPool chunks on x64

--- public/tier1/mempool.h
+++ public/tier1/mempool.h
@@ -462,7 +462,8 @@ inline void CAlignedMemPool<ITEM_SIZE, ALIGNMENT, CHUNK_SIZE, CAllocator, COMPAC
 template <int ITEM_SIZE, int ALIGNMENT, int CHUNK_SIZE, class CAllocator, int COMPACT_THRESHOLD >
 inline int __cdecl CAlignedMemPool<ITEM_SIZE, ALIGNMENT, CHUNK_SIZE, CAllocator, COMPACT_THRESHOLD>::CompareChunk( void * const *ppLeft, void * const *ppRight )
 {
-	return ((unsigned)*ppLeft) - ((unsigned)*ppRight);
+	// dimhotepus: Correctly compare chunks on x86-64.
+	return ((uintp)*ppLeft) - ((uintp)*ppRight);
 }
 
 template <int ITEM_SIZE, int ALIGNMENT, int CHUNK_SIZE, class CAllocator, int COMPACT_THRESHOLD >
