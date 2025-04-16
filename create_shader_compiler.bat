efdf1ae2e051fbeba50aa4bf7400f01e86c2c846
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-04-17 01:18:41 +0300

    [shadercompile] Create shader compiler project script should honor dependencies

--- create_shader_compiler.bat
+++ create_shader_compiler.bat
@@ -22,25 +22,82 @@ if ["%~2"]==["x86"] (
   SET CMAKE_MSVC_ARCH_NAME=Win32
   SET MSBUILD_PLATFORM=x86
 
-  ECHO "Generating solution %SOLUTION_NAME for x86"
+  ECHO "Generating solution %SOLUTION_NAME% for x86"
   SET "SOLUTION_NAME=%SOLUTION_NAME%_x86"
 ) else if ["%~2"]==["x64"] (
   SET CMAKE_MSVC_ARCH_NAME=x64
   SET MSBUILD_PLATFORM=x64
 
-  ECHO "Generating solution %SOLUTION_NAME_x64 for x64"
+  ECHO "Generating solution %SOLUTION_NAME%_x64 for x64"
   SET "SOLUTION_NAME=%SOLUTION_NAME%_x64"
 ) else (
   ECHO Please pass CPU architecture as second argument. Supported x86 and x64.
   EXIT /B 1
 )
 
-REM Build VPC.
-MSBuild.exe /m /p:Platform=x64 /p:Configuration=Release external/vpc/vpc.sln
+REM Set CMake / MSVC generator / architecture / platform.
+SET CMAKE_MSVC_GEN_NAME="Visual Studio 17 2022"
+
+
+REM Build cryptopp.
+MSBuild.exe /m /p:Platform=%MSBUILD_PLATFORM% /p:Configuration=Debug thirdparty\cryptopp\cryptlib.vcxproj
 if ERRORLEVEL 1 (
-  ECHO MSBuild Release x64 for external/vpc/vpc.sln failed.
+  ECHO MSBuild Debug for thirdparty\cryptopp\cryptlib failed.
   EXIT /B 1
 )
+
+MSBuild.exe /m /p:Platform=%MSBUILD_PLATFORM% /p:Configuration=Release thirdparty\cryptopp\cryptlib.vcxproj
+if ERRORLEVEL 1 (
+  ECHO MSBuild Release for thirdparty\cryptopp\cryptlib failed.
+  EXIT /B 1
+)
+
+
+REM Build zlib.
+MKDIR thirdparty\zlib\out
+PUSHD thirdparty\zlib\out
+cmake -G %CMAKE_MSVC_GEN_NAME% -A %CMAKE_MSVC_ARCH_NAME% ..
+if ERRORLEVEL 1 (
+  ECHO cmake generation for thirdparty\zlib failed.
+  EXIT /B 1
+)
+
+cmake --build . --config Release
+if ERRORLEVEL 1 (
+  ECHO cmake --build for thirdparty\zlib failed.
+  EXIT /B 1
+)
+POPD
+
+
+REM Build VPC.
+MKDIR external\vpc\out
+PUSHD external\vpc\out
+cmake -G Ninja ..
+if ERRORLEVEL 1 (
+  ECHO cmake generation for external\vpc failed.
+  EXIT /B 1
+)
+
+cmake --build . --config Release
+if ERRORLEVEL 1 (
+  ECHO cmake --build for external\vpc failed.
+  EXIT /B 1
+)
+
+COPY /V /Y vpc.exe /B ..\..\..\devtools\bin\vpc.exe /B
+if ERRORLEVEL 1 (
+  ECHO No vpc.exe to copy to devtools\bin\.
+  EXIT /B 1
+)
+
+COPY /V /Y vpc.pdb /B ..\..\..\devtools\bin\vpc.pdb /B
+if ERRORLEVEL 1 (
+  ECHO No vpc.pdb to copy to devtools\bin\.
+  EXIT /B 1
+)
+POPD
+
 
 SET "WIN_X64= "
 if ["%CMAKE_MSVC_ARCH_NAME%"]==["x64"] (
@@ -48,6 +105,17 @@ if ["%CMAKE_MSVC_ARCH_NAME%"]==["x64"] (
 
   echo Add /windows arg to VPC as generating x64 solution. 
 )
+
+
+REM Generate version info, etc.
+MKDIR out
+PUSHD out
+cmake ..\CMakeLists.txt
+if ERRORLEVEL 1 (
+  ECHO cmake version generation failed.
+  EXIT /B 1
+)
+POPD
 
 
 REM Finally create solution.
