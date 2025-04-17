Source-Authors/Obsoletium

commit 0330609be1686f365f1141d1f7b82b7bbf88fd19

[vphysics] Use for-range loop to clear dead objects

vphysics/physics_environment.cpp
@@ -1707,12 +1707,10 @@ void CPhysicsEnvironment::SetCollisionSolver( IPhysicsCollisionSolver *pSolver )
 
 void CPhysicsEnvironment::ClearDeadObjects( void )
 {
-	for ( intp i = 0; i < m_deadObjects.Count(); i++ )
+	for ( auto *o : m_deadObjects )
 	{
-		CPhysicsObject *pObject = (CPhysicsObject *)m_deadObjects.Element(i);
-
-		m_pSleepEvents->DeleteObject( pObject );
-		delete pObject;
+		m_pSleepEvents->DeleteObject( (CPhysicsObject *)o );
+		delete o;
 	}
 	m_deadObjects.Purge();
 	m_pDeleteQueue->DeleteAll();

commit 78b72ab4c18c6f97ad4c04ddda00ba63623caf36

[vphysics] Fix regression found by AddressSanitizer
Introduced in 0330609be1686f365f1141d1f7b82b7bbf88fd19

vphysics/physics_environment.cpp
@@ -1707,10 +1707,14 @@ void CPhysicsEnvironment::SetCollisionSolver( IPhysicsCollisionSolver *pSolver )
 
 void CPhysicsEnvironment::ClearDeadObjects( void )
 {
-	for ( auto *o : m_deadObjects )
+	// dimhotepus: Can't use range-for loop here as DeleteObject may recursively
+	// call ClearDeadObjects and m_deadObjects will be modified when iterating.
+	for ( intp i = 0; i < m_deadObjects.Count(); i++ )
 	{
-		m_pSleepEvents->DeleteObject( (CPhysicsObject *)o );
-		delete o;
+		auto *pObject = (CPhysicsObject *)m_deadObjects.Element(i);
+
+		m_pSleepEvents->DeleteObject( pObject );
+		delete pObject;
 	}
 	m_deadObjects.Purge();
 	m_pDeleteQueue->DeleteAll();