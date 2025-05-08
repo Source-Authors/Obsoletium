9b04018c6ae254abc2a45dc206f7a8394a433a9b
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-05-09 01:05:51 +0300

    [motionmapper] Faster string emptiness check

--- utils/motionmapper/motionmapper.cpp
+++ utils/motionmapper/motionmapper.cpp
@@ -247,7 +247,7 @@ int Grab_Nodes(s_node_t *pnodes) {
     if (sscanf(g_szLine, "%d \"%1023[^\"]\" %d", &index, name, &parent) == 3) {
       // check for duplicated bones
       /*
-      if (strlen(pnodes[index].name) != 0)
+      if (!Q_isempty(pnodes[index].name))
       {
               MdlError( "bone \"%s\" exists more than once\n", name );
       }
