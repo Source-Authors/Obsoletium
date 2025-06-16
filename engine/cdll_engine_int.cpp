78642ed68fe05c8f49479e67c00fcb65405f74a5
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-06-16 16:31:52 +0300

    engine/client: Require GPU to support ps1.1 and be DirectX8.0+ hardware for all games

--- engine/cdll_engine_int.cpp
+++ engine/cdll_engine_int.cpp
@@ -1912,13 +1912,9 @@ void ClientDLL_Init( void )
 			toolframework->ClientInit( g_ClientFactory );
 		}
 
-		// Don't want TF2 running less than DX 8
 		if ( g_pMaterialSystemHardwareConfig && g_pMaterialSystemHardwareConfig->GetDXSupportLevel() < 80 )
 		{
-			if ( ( Q_stricmp( COM_GetModDirectory(), "tf" ) == 0 ) || ( Q_stricmp( COM_GetModDirectory(), "ep2" ) == 0 ) || ( Q_stricmp( COM_GetModDirectory(), "portal" ) == 0 ) || ( Q_stricmp( COM_GetModDirectory(), "tf_beta" ) == 0 ) )
-			{
-				Sys_Error( "Your graphics hardware must support at least pixel shader version 1.1 to run this game!" );
-			}
+			Sys_Error( "Your GPU must support at least pixel shader version 1.1 to run this game!" );
 		}
 	}
 
--- game/client/viewrender.cpp
+++ game/client/viewrender.cpp
@@ -1918,13 +1918,8 @@ void CViewRender::RenderView( const CViewSetup &viewRender, int nClearFlags, int
 	VPROF( "CViewRender::RenderView" );
 	tmZone( TELEMETRY_LEVEL0, TMZF_NONE, "%s", __FUNCTION__ );
 
-	// Don't want TF2 running less than DX 8
 	if ( g_pMaterialSystemHardwareConfig->GetDXSupportLevel() < 80 )
 	{
-		// We know they were running at least 8.0 when the game started...we check the 
-		// value in ClientDLL_Init()...so they must be messing with their DirectX settings.
-		if ( ( Q_stricmp( COM_GetModDirectory(), "tf" ) == 0 ) || ( Q_stricmp( COM_GetModDirectory(), "tf_beta" ) == 0 ) )
-		{
 			static bool bFirstTime = true;
 			if ( bFirstTime )
 			{
@@ -1933,7 +1928,6 @@ void CViewRender::RenderView( const CViewSetup &viewRender, int nClearFlags, int
 			}
 			return;
 		}
-	}
 
 	CMatRenderContextPtr pRenderContext( materials );
 	ITexture *saveRenderTarget = pRenderContext->GetRenderTarget();
