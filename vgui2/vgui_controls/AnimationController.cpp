f53edf21eb675f767bac774ce4a94f4e7d4148db
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-07-26 15:26:01 +0300

    vgui_controls: x86-64 file length

--- vgui2/vgui_controls/AnimationController.cpp
+++ vgui2/vgui_controls/AnimationController.cpp
@@ -284,7 +284,7 @@ void AnimationController::SetupPosition( AnimCmdAnimate_t& cmd, float *output, c
 //-----------------------------------------------------------------------------
 // Purpose: parses a script into sequences
 //-----------------------------------------------------------------------------
-bool AnimationController::ParseScriptFile(char *pMem, int length)
+bool AnimationController::ParseScriptFile(char *pMem, intp length)
 {
 	// get the scheme (for looking up color names)
 	IScheme *scheme = vgui::scheme()->GetIScheme(GetScheme());
