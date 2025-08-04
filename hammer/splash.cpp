805b25d44ce3d92b560b045a6eea8e8ad419b194
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-08-04 18:54:13 +0300

    hammer: Scale splash window by DPI

--- hammer/splash.cpp
+++ hammer/splash.cpp
@@ -6,6 +6,7 @@
 
 #include "stdafx.h"
 #include "Splash.h"
+#include "windows/bitmap_scale.h"
 
 #include "resource.h"
 
@@ -300,7 +301,22 @@ BOOL CSplashWnd::Create(CWnd* pParentWnd /*= NULL*/)
 	if (!m_bitmap.LoadBitmap(IDB_SPLASH))
 		return FALSE;
 
+	const unsigned dpi = ::GetDpiForWindow(pParentWnd->GetSafeHwnd());
+
 	BITMAP bm;
+    m_bitmap.GetBitmap(&bm);
+
+	HBITMAP scaledBitmap = se::windows::ui::ScaleBitmapForDpi(
+		static_cast<HBITMAP>(m_bitmap.GetSafeHandle()),
+		USER_DEFAULT_SCREEN_DPI,
+		USER_DEFAULT_SCREEN_DPI,
+		dpi,
+		dpi);
+
+	// destroy old.
+	m_bitmap.DeleteObject();
+	// attach makes owner, no need to delete.
+	m_bitmap.Attach(scaledBitmap);
 	m_bitmap.GetBitmap(&bm);
 
 	return CreateEx(0,
