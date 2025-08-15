86752552e2b952dc21f8266a55cdaaae5881efd1
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-08-15 14:29:57 +0300

    mathlib: Precompute VERTEXNORMAL_CONE_INNER_ANGLE during compile time

--- public/mathlib/anorms.h
+++ public/mathlib/anorms.h
@@ -11,13 +11,14 @@
 #endif
 
 
+#include "mathlib/mathlib.h"
 #include "mathlib/vector.h"
 
 
 #define NUMVERTEXNORMALS	162
 
 // the angle between consecutive g_anorms[] vectors is ~14.55 degrees
-#define VERTEXNORMAL_CONE_INNER_ANGLE	DEG2RAD(7.275f)
+constexpr inline float VERTEXNORMAL_CONE_INNER_ANGLE{DEG2RAD(7.275f)};
 
 extern Vector g_anorms[NUMVERTEXNORMALS];
 
