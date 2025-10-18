Source-Authors/Obsoletium

commit fbe5289ba1b82b517db5653e2942bcc3edb8bfb9

engine: Do a type-safe copying of messages

engine/tmessage.cpp
@@ -602,7 +602,7 @@ void TextMessage_DemoMessageFull( const char *pszMessage, client_textmessage_t c
 	if ( Q_isempty( pszMessage ) )
 		return;
 
-	memcpy( &tm_demomessage, message, sizeof( tm_demomessage ) );
+	BitwiseCopy( message, &tm_demomessage, sizeof( tm_demomessage ) );
 	tm_demomessage.pMessage = orig_demo_message.pMessage;
 	tm_demomessage.pName = orig_demo_message.pName;
 	V_strcpy_safe( gDemoMessageBuffer, pszMessage );

commit 69596a2815b7fc5c9cdffe3d6fa2ef7ed154e683

engine: Fix introduced buffer overflow
Regression from fbe5289ba1b82b517db5653e2942bcc3edb8bfb9

engine/tmessage.cpp
@@ -599,7 +599,7 @@ void TextMessage_DemoMessageFull( const char *pszMessage, client_textmessage_t c
 	if ( Q_isempty( pszMessage ) )
 		return;
 
-	BitwiseCopy( message, &tm_demomessage, sizeof( tm_demomessage ) );
+	BitwiseCopy( message, &tm_demomessage, 1 );
 	tm_demomessage.pMessage = orig_demo_message.pMessage;
 	tm_demomessage.pName = orig_demo_message.pName;
 	V_strcpy_safe( gDemoMessageBuffer, pszMessage );