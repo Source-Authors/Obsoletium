7f16cc33c06ba6aeb96869c7cafa63af318c757d
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-10-19 05:37:12 +0300

    vgui_controls: Message box should have owner to scale UI

--- vgui2/vgui_controls/BuildModeDialog.cpp
+++ vgui2/vgui_controls/BuildModeDialog.cpp
@@ -965,7 +965,8 @@ void BuildModeDialog::ApplyDataToControls()
 		{
 			char messageString[255];
 			Q_snprintf(messageString, sizeof( messageString ), "Fieldname is not unique: %s\nRename it and try again.", fieldName);
-			MessageBox *errorBox = new MessageBox("Cannot Apply", messageString);
+			// dimhotepus: Own message box to scale it.
+			MessageBox *errorBox = new MessageBox("Cannot Apply", messageString, this);
 			errorBox->DoModal();
 			UpdateControlData(m_pCurrentPanel);
 			m_pApplyButton->SetEnabled(false);
