95d84546931af113cbc9a0e232d8e3f9bcf72d85
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-10-21 03:41:47 +0300

    tier0: Fix warnings about float <-> double mismatch

--- tier0/cpumonitoring.cpp
+++ tier0/cpumonitoring.cpp
@@ -190,10 +190,10 @@ unsigned __stdcall HeartbeatThread( void* )
 			{
 				// Read and write all the state that is shared with the main thread while holding the lock.
 				AUTO_LOCK( s_lock );
-				float freqPercentage = maxActualFreq / (MaxCurrentMHz * 1e-5f) / 1e9;
+				float freqPercentage = maxActualFreq / (MaxCurrentMHz * 1e-5f) / 1e9f;
 				constexpr float kFudgeFactor = 1.03f;	// Make results match reality better
 				s_results.m_timeStamp = Plat_FloatTime();
-				s_results.m_GHz = maxActualFreq * kFudgeFactor / 1e9;
+				s_results.m_GHz = maxActualFreq * kFudgeFactor / 1e9f;
 				s_results.m_percentage = freqPercentage * kFudgeFactor;
 
 				if ( s_results.m_lowestPercentage == 0 || s_results.m_percentage < s_results.m_lowestPercentage )
