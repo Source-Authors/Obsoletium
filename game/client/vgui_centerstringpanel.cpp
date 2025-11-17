c0dcaf89620b2e4198857492eac304ad051243fa
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-11-17 14:08:45 +0300

    client: Use typed cursor instead of magic 0 one

--- game/client/vgui_centerstringpanel.cpp
+++ game/client/vgui_centerstringpanel.cpp
@@ -72,7 +72,7 @@ CCenterStringLabel::CCenterStringLabel( vgui::VPANEL parent ) :
 	SetParent( parent );
 	ComputeSize();
 	SetVisible( false );
-	SetCursor( 0 );
+	SetCursor( vgui::dc_user );
 	SetKeyBoardInputEnabled( false );
 	SetMouseInputEnabled( false );
 	SetContentAlignment( vgui::Label::a_center );
