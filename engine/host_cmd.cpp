5cc439240f659eeee5e7c440e0a94223d5204abc
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-11-17 19:43:48 +0100

    engine: prevent kicking of host

--- engine/host_cmd.cpp
+++ engine/host_cmd.cpp
@@ -1249,6 +1249,12 @@ CON_COMMAND( kickid, "Kick a player by userid or uniqueid, with a message." )
 			return;
 		}
 
+		if ( host_client == client )
+		{
+			ConMsg( "Preventing kick of local server host!\n" );
+			return;
+		}
+
 		if ( iSearchIndex != -1 || !client->IsFakeClient() )
 		{
 			if ( who == NULL )
@@ -1359,6 +1365,12 @@ CON_COMMAND( kick, "Kick a player by name." )
 			if ( cmd_source != src_command && host_client == client && !sv.IsDedicated() )
 				return;
 
+			if ( host_client == client )
+			{
+				ConMsg( "Preventing kick of local server host!\n" );
+				return;
+			}
+
 			if ( who )
 			{
 				client->Disconnect( "Kicked by %s", who );
@@ -1412,6 +1424,12 @@ CON_COMMAND( kickall, "Kicks everybody connected with a message." )
 
 		if ( client->IsHLTV() )
 			continue;
+
+		if ( host_client == client )
+		{
+			ConMsg( "Preventing kick of local server host!\n" );
+			continue;
+		}
 
 		if ( who )
 		{
