1a54c1b39378d88425d2e18fe2f3713f716f6074
Dmitry Tsarevich <dimhotepus@gmail.com>
2025-11-20 02:21:19 +0300

    server\episodic: Do not mix float and double

--- game/server/episodic/npc_hunter.cpp
+++ game/server/episodic/npc_hunter.cpp
@@ -3728,7 +3728,7 @@ void CNPC_Hunter::RunTask( const Task_t *pTask )
 				{
 					m_flNextChargeTime = gpGlobals->curtime + hunter_charge_min_delay.GetFloat() + random->RandomFloat( 0, 2.5 ) + random->RandomFloat( 0, 2.5 );
 					float delayMultiplier = ( g_pGameRules->IsSkillLevel( SKILL_EASY ) ) ? 1.5f : 1.0f;
-					float groupDelay = gpGlobals->curtime +  ( 2.0  + random->RandomFloat( 0, 2 ) ) * delayMultiplier;
+					float groupDelay = gpGlobals->curtime +  ( 2.0f  + random->RandomFloat( 0, 2 ) ) * delayMultiplier;
 					for ( int i = 0; i < g_Hunters.Count(); i++ )
 					{
 						if ( g_Hunters[i] != this && g_Hunters[i]->m_flNextChargeTime < groupDelay )
