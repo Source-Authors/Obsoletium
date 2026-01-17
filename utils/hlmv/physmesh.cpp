9e07f19fda3970ebebd656c1d4fd7c99a98f66fa
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-01-17 06:03:01 +0300

    hlmv: Use default ctor / dtor

--- utils/hlmv/physmesh.cpp
+++ utils/hlmv/physmesh.cpp
@@ -375,8 +375,8 @@ intp FindPhysprop( const char *pPropname )
 class CTextBuffer
 {
 public:
-	CTextBuffer( void ) {}
-	~CTextBuffer( void ) {}
+	CTextBuffer( void ) = default;
+	~CTextBuffer( void ) = default;
 
 	inline intp GetSize( void ) { return m_buffer.Count(); }
 	inline char *GetData( void ) { return m_buffer.Base(); }
