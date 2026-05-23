1d21a87a7ce96d89723205ec8abdebfba9a1c0e9
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-05-23 10:53:02 +0300

    engine: Correctly compute MP3 file length in mp3 player

--- engine/audio/private/mp3duration_helper.cpp
+++ engine/audio/private/mp3duration_helper.cpp
@@ -14,7 +14,77 @@
 
 #include "memdbgon.h"
 
-extern IFileSystem *g_pFullFileSystem;
+extern IFileSystem* g_pFullFileSystem;
+
+namespace
+{
+
+[[nodiscard]] FileSystemSeek_t MapMPAFileSeekType(MPAFileSeekType seek_type)
+{
+	switch (seek_type)
+	{
+		case MPAFileSeekType::kCurrent:
+			return FileSystemSeek_t::FILESYSTEM_SEEK_CURRENT;
+		case MPAFileSeekType::kEnd:
+			return FileSystemSeek_t::FILESYSTEM_SEEK_TAIL;
+		case MPAFileSeekType::kSet:
+			return FileSystemSeek_t::FILESYSTEM_SEEK_HEAD;
+		default:
+			return FileSystemSeek_t::FILESYSTEM_SEEK_CURRENT;
+	}
+}
+	
+class SourceMPAFile : public IMPAFile
+{
+public:
+	SourceMPAFile(const char* path,	const char* mode, IFileSystem* file_system)
+		: m_file{file_system->Open(path, mode)},
+		m_file_system{file_system},
+		m_size(file_system->Size(m_file))
+	{
+	}
+	~SourceMPAFile()
+	{
+		if (m_file)
+		{
+			m_file_system->Close(m_file);
+		}
+	}
+
+	[[nodiscard]] size_t Read(void* buffer, size_t size) override
+	{
+		return m_file_system->Read(buffer, size, m_file);
+	}
+
+	[[nodiscard]] int Seek(long offset, MPAFileSeekType seek_type) override
+	{
+		m_file_system->Seek(m_file, offset, MapMPAFileSeekType(seek_type));
+		return 0;
+	}
+	[[nodiscard]] long Tell() override
+	{
+		return m_file_system->Tell(m_file);
+	}
+	
+	[[nodiscard]] int Error() override
+	{
+		return m_file_system->IsOk(m_file) ? 0 : 1;
+	}
+
+private:
+	FileHandle_t m_file;
+	IFileSystem *m_file_system;
+	const unsigned m_size;
+};
+
+struct SourceMPAFileSystem : public IMPAFileSystem
+{
+	[[nodiscard]] std::unique_ptr<IMPAFile> Open(const char* path,
+		const char* mode) override
+	{
+		return std::make_unique<SourceMPAFile>(path, mode, g_pFullFileSystem);
+	}
+};
 
 struct MP3Duration_t
 {
@@ -29,13 +99,13 @@ struct MP3Duration_t
 
 CUtlRBTree< MP3Duration_t, int >    g_MP3Durations( 0, 0, MP3Duration_t::LessFunc );
 
+}
+
 float GetMP3Duration_Helper( char const *filename )
 {
-	float duration = 60.0f;
-
 	// See if it's in the RB tree already...
 	char fn[ MAX_PATH ];
-	Q_snprintf( fn, sizeof( fn ), "sound/%s", PSkipSoundChars( filename ) );
+	V_sprintf_safe( fn, "sound/%s", PSkipSoundChars( filename ) );
 	V_FixSlashes(fn);
 
 	MP3Duration_t search = {};
@@ -50,10 +120,13 @@ float GetMP3Duration_Helper( char const *filename )
 	char fullPath[MAX_PATH];
 	fullPath[0] = '\0';
 	g_pFullFileSystem->RelativePathToFullPath_safe( fn, "MOD", fullPath );
+	
+	float duration = 60.0f;
+	SourceMPAFileSystem source_file_system;
 
 	try
 	{
-		CMPAFile MPAFile( !Q_isempty( fullPath ) ? fullPath : fn );
+		CMPAFile MPAFile( !Q_isempty( fullPath ) ? fullPath : fn, &source_file_system );
 		duration = (float)MPAFile.GetLengthSec();
 	}
 	catch ( std::exception &e )
