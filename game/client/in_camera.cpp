3c65865b549990e1132cd68952a4cab6a81571f8
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-05-29 03:31:34 +0300

    game/client: Fix flipped initial pitch/yaw on thirdperson switch
    See https://github.com/ValveSoftware/source-sdk-2013/pull/1791

--- game/client/in_camera.cpp
+++ game/client/in_camera.cpp
@@ -683,7 +683,8 @@ void CInput::CAM_ToThirdPerson(void)
 	{
 		m_fCameraInThirdPerson = true; 
 	
-		g_ThirdPersonManager.SetCameraOffsetAngles( Vector( viewangles[ YAW ], viewangles[ PITCH ], CAM_MIN_DIST ) );
+		// dimhotepus: Fix flipped initial pitch/yaw on thirdperson switch (ficool).
+		g_ThirdPersonManager.SetCameraOffsetAngles( Vector( viewangles[ PITCH ], viewangles[ YAW ], CAM_MIN_DIST ) );
 	}
 
 	cam_command.SetValue( 0 );
