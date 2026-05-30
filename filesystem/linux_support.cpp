5e6d33695666bb6d982e91acd89d4cec5d91b076
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-05-30 15:22:01 +0300

    filesystem: Cleanup linux support
    * toupper -> V_toupper to correctly handle signed chars.
    * thread-safe findFileInDirCaseInsensitive.
    * nullptr after free to prevent UAF.
    * RAII for closedir.

--- filesystem/linux_support.cpp
+++ filesystem/linux_support.cpp
@@ -6,132 +6,128 @@
 //=============================================================================//
 
 #include "linux_support.h"
-#include "tier0/threadtools.h" // For ThreadInMainThread()
+
+#include <sys/stat.h> // stat()
+#include <unistd.h> 
+#include <dirent.h> // scandir()
+
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+
 #include "tier1/strtools.h"
 
-char selectBuf[PATH_MAX];
-
-int FileSelect(const struct dirent *ent)
+namespace 
 {
-	const char *mask=selectBuf;
-	const char *name=ent->d_name;
 
-	//printf("Test:%s %s\n",mask,name);
+// dimhotepus: Make thread_local to support multiple threads.
+thread_local char selectBuf[PATH_MAX];
 
-	if(V_streq(name,".") || V_streq(name,"..") ) return 0;
+int FileSelect( const dirent *ent )
+{
+	const char *mask{selectBuf}, *name{ent->d_name};
 
-	if(V_streq(selectBuf,"*.*")) return 1;
+	if (V_streq(name, ".") || V_streq(name, "..") ) return 0;
+	if (V_streq(selectBuf, "*.*")) return 1;
 
-	while( *mask && *name )
+	while (*mask && *name)
 	{
-		if(*mask=='*')
+		if (*mask == '*')
 		{
 			mask++; // move to the next char in the mask
-			if(!*mask) // if this is the end of the mask its a match 
+
+			// if this is the end of the mask its a match
+			if (!*mask) return 1;
+
+			// dimhotepus: toupper -> V_toupper.
+			// while the two don't meet up again
+			while (*name && V_toupper(*name) != V_toupper(*mask)) 
 			{
-				return 1;
-			}
-			while(*name && toupper(*name)!=toupper(*mask)) 
-			{ // while the two don't meet up again
 				name++;
-			}
-			if(!*name) 
-			{ // end of the name
-				break; 
-			}
-		}
-		else if (*mask!='?')
-		{
-			if( toupper(*mask) != toupper(*name) )
-			{	// mismatched!
-				return 0;
-			}
-			else
-			{	
-				mask++;
-				name++;
-				if( !*mask && !*name) 
-				{ // if its at the end of the buffer
-					return 1;
-				}
-				
 			}
 
+			// end of the name
+			if (!*name) break; 
+		}
+		else if (*mask != '?')
+		{
+			// dimhotepus: toupper -> V_toupper.
+			// mismatched!
+			if (V_toupper(*mask) != V_toupper(*name)) return 0;
+
+			mask++;
+			name++;
+
+			// if its at the end of the buffer
+			if (!*mask && !*name) return 1;
 		}
 		else /* mask is "?", we don't care*/
 		{
 			mask++;
 			name++;
 		}
-	}	
-		
-	return( !*mask && !*name ); // both of the strings are at the end
+	}
+
+	// both of the strings are at the end
+	return !*mask && !*name;
 }
 
-int FillDataStruct(FIND_DATA *dat)
+int FillDataStruct( FIND_DATA *dat )
 {
-	struct stat fileStat;
-
-	if(dat->curMatch >= dat->numMatches)
+	if (dat->curMatch >= dat->numMatches)
 		return -1;
 
-	char szFullPath[MAX_PATH];
-	Q_snprintf( szFullPath, sizeof(szFullPath), "%s/%s", dat->cBaseDir, dat->namelist[dat->curMatch]->d_name );  
+	auto *name = dat->namelist[dat->curMatch];
 
-	if(!stat(szFullPath,&fileStat))
-	{
-		dat->dwFileAttributes=fileStat.st_mode;           
-	}
-	else
-	{
-		dat->dwFileAttributes=0;
-	}	
+	char szFullPath[MAX_PATH];
+	V_sprintf_safe( szFullPath, "%s/%s", dat->cBaseDir, name->d_name );  
+
+	stat fileStat;
+	dat->dwFileAttributes = !stat( szFullPath, &fileStat ) ? fileStat.st_mode : 0;           
 
 	// now just put the filename in the output data
-	Q_snprintf( dat->cFileName, sizeof(dat->cFileName), "%s", dat->namelist[dat->curMatch]->d_name );  
+	V_sprintf_safe( dat->cFileName, "%s", name->d_name );  
 
-	//printf("%s\n", dat->namelist[dat->curMatch]->d_name);
-	free(dat->namelist[dat->curMatch]);
+	free( name );
 
-	dat->curMatch++;
+	++dat->curMatch;
 	return 1;
 }
 
+}  // namespace
 
-HANDLE FindFirstFile( const char *fileName, FIND_DATA *dat)
+HANDLE FindFirstFile( const char *fileName, FIND_DATA *dat )
 {
 	char nameStore[PATH_MAX];
-	char *dir=NULL;
-	int n,iret=-1;
+	char *dir = nullptr;
+	int iret=-1;
 	
-	Q_strncpy(nameStore,fileName, sizeof( nameStore ) );
+	V_strcpy_safe(nameStore, fileName );
 
-	if(strrchr(nameStore,'/') )
+	if (strrchr(nameStore, '/'))
 	{
-		dir=nameStore;
-		while(strrchr(dir,'/') )
+		dir = nameStore;
+		while (strrchr(dir, '/'))
 		{
-			struct stat dirChk;
-
 			// zero this with the dir name
-			dir=strrchr(nameStore,'/');
-			if ( dir == nameStore ) // special case for root dir, '/'
+			dir = strrchr(nameStore, '/');
+			if (dir == nameStore) // special case for root dir, '/'
 			{
 				dir[1] = '\0';
 			}
 			else
 			{
 				*dir='\0';
-				dir=nameStore;
+				dir = nameStore;
 			}
-
 			
-			if (stat(dir,&dirChk) < 0)
+			stat dirChk;
+			if (stat(dir, &dirChk) < 0)
 			{
 				continue;
 			}
 
-			if( S_ISDIR( dirChk.st_mode ) )
+			if (S_ISDIR(dirChk.st_mode))
 			{
 				break;	
 			}
@@ -143,56 +139,64 @@ HANDLE FindFirstFile( const char *fileName, FIND_DATA *dat)
 		return (HANDLE)-1;
 	}
 
-	if( !Q_isempty(dir) )
+	if (!Q_isempty(dir))
 	{
-		if ( strlen(dir) == 1 ) // if it was the root dir
-			Q_strncpy(selectBuf,fileName+1, sizeof( selectBuf ) );
-		else
-			Q_strncpy(selectBuf,fileName+strlen(dir)+1, sizeof( selectBuf ) );
+		// dimhotepus: Speedup root dir computation.
+		const bool isRoot = !Q_isempty(dir) && dir[0] == '/' && dir[1] == '\0';
+		V_strcpy_safe(selectBuf, fileName + (isRoot ? 0 : 1) + 1);
 
-		Q_strncpy(dat->cBaseDir,dir, sizeof( dat->cBaseDir ) );
-		dat->namelist = NULL;
-		n = scandir(dir, &dat->namelist, FileSelect, alphasort);
-		if (n < 0)
+		dat->namelist = nullptr;
+		V_strcpy_safe(dat->cBaseDir, dir);
+
+		const int n = scandir(dir, &dat->namelist, FileSelect, alphasort);
+		if ( n < 0 )
 		{
 			if ( dat->namelist )
+			{
 				free(dat->namelist);
+				// dimhotepus: Set to nullptr to prefent UAF.
+				dat->namelist = nullptr;
+			}
 			// silently return, nothing interesting
 		}
 		else 
 		{
 			dat->numMatches = n;
 			dat->curMatch = 0;
-			iret=FillDataStruct(dat);
-			if(iret<0)
+
+			iret = FillDataStruct(dat);
+			if ( iret < 0 )
 			{
 				if ( dat->namelist )
+				{
 					free(dat->namelist);
-				dat->namelist = NULL;
+					dat->namelist = nullptr;
+				}
 			}
-
 		}
 	}
 
-//	printf("Returning: %i \n",iret);
 	return (HANDLE)iret;
 }
 
-bool FindNextFile(HANDLE handle, FIND_DATA *dat)
+bool FindNextFile( HANDLE handle, FIND_DATA *dat )
 {
-	if(dat->curMatch >= dat->numMatches)
+	if ( dat->curMatch >= dat->numMatches )
 	{	
-		if ( dat->namelist != NULL )
-			free(dat->namelist);
-		dat->namelist = NULL;
-		return false; // no matches left
-	}	
+		if ( dat->namelist != nullptr )
+		{
+			free( dat->namelist );
+			dat->namelist = nullptr;
+		}
 
-	FillDataStruct(dat);
-	return true;
+		return false; // no matches left
+	}
+
+	// dimhotepus: Report false if fill failed.
+	return FillDataStruct( dat ) > 0;
 }
 
-bool FindClose(HANDLE handle)
+bool FindClose( HANDLE handle )
 {
 	return true;
 }
@@ -203,39 +207,42 @@ bool FindClose(HANDLE handle)
 // directory that match the file name but potentially with different case.
 // The directory name itself is not treated specially.
 // If multiple names that match are found then lowercase letters take precedence.
-bool findFileInDirCaseInsensitive( const char *file, char* output, size_t bufSize)
+bool findFileInDirCaseInsensitive( IN_Z const char *file, OUT_Z_BYTECAP(bufSize) char* output, size_t bufSize )
 {
 	// Make sure the output buffer is always null-terminated.
-	output[0] = 0;
+	if (bufSize > 0)
+		output[0] = '\0';
 
 	// Find where the file part starts.
 	const char *dirSep = strrchr(file,'/');
-	if( !dirSep )
+	if ( !dirSep )
 	{
-		dirSep=strrchr(file,'\\');
-		if( !dirSep ) 
+		dirSep = strrchr(file, '\\');
+		if ( !dirSep ) 
 		{
 			return false;
 		}
 	}
 
 	// Allocate space for the directory portion.
-	size_t dirSize = ( dirSep - file ) + 1;
-	char *dirName = static_cast<char *>( alloca( dirSize ) ); 
+	size_t dirSize = (dirSep - file) + 1;
+	char *dirName = stackallocT( char, dirSize ); 
 
-	V_strncpy( dirName , file, dirSize );
+	V_strncpy( dirName, file, dirSize );
 
 	DIR* pDir = opendir( dirName );
 	if ( !pDir )
 		return false;
 
+	RunCodeAtScopeExit(closedir( pDir ));
+
 	const char* filePart = dirSep + 1;
 	// The best matching file name will be placed in this array.
-	char outputFileName[ MAX_PATH ];
+	char outputFileName[MAX_PATH];
 	bool foundMatch = false;
 
 	// Scan through the directory.
-	for ( dirent* pEntry = NULL; ( pEntry = readdir( pDir ) ); /**/ )
+	for ( dirent* pEntry = nullptr; pEntry = readdir( pDir ); /**/ )
 	{
 		if ( V_strieq( pEntry->d_name, filePart ) )
 		{
@@ -243,15 +250,13 @@ bool findFileInDirCaseInsensitive( const char *file, char* output, size_t bufSiz
 			// a better candidate then copy it in. A 'better' candidate
 			// means that test beats tesT which beats tEst -- more lowercase
 			// letters earlier equals victory.
-			if ( !foundMatch || strcmp( outputFileName, pEntry->d_name ) < 0 )
+			if ( !foundMatch || V_strcmp( outputFileName, pEntry->d_name ) < 0 )
 			{
 				foundMatch = true;
 				V_strcpy_safe( outputFileName, pEntry->d_name );
 			}
 		}
 	}
-
-	closedir( pDir );
 
 	// If we didn't find any matching names then lowercase the passed in
 	// file name and use that.
