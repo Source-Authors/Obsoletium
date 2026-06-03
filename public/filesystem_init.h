67ce6f6312c5696f6b1c38b0c367dffab8a699dc
Dmitry Tsarevich <dimhotepus@gmail.com>
2026-06-03 14:05:21 +0300

    common: Do not hide outSize from inner by outer template

--- public/filesystem_init.h
+++ public/filesystem_init.h
@@ -226,7 +226,7 @@ void FileSystem_ClearSteamEnvVars();
 template <int outSize>
 [[nodiscard]] bool Q_getwd(OUT_Z_ARRAY char (&out)[outSize])
 {
-	[[nodiscard]] bool Q_getwd(OUT_Z_CAP(outSize) char *out, int outSize);
+	[[nodiscard]] bool Q_getwd(OUT_Z_CAP(outSize) char *out, int size);
 	return Q_getwd( out, outSize );
 }
 
