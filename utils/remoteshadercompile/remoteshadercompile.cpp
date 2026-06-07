Source-Authors/Obsoletium

commit 145ebd224f9c96c3c8c4cf2976f7ae1cf0bb9ac0

all: !strcmp -> V_streq

utils/remoteshadercompile/remoteshadercompile.cpp
@@ -20,6 +20,7 @@
 
 #include "windows/com_error_category.h"
 #include "tier0/dbg.h"
+#include "tier1/strtools.h"
 
 #include "tier0/memdbgon.h"
 
@@ -780,7 +781,7 @@ int main(int argc, char *argv[]) {
 
   bool show_logo{true};
   for (int i{1}; i < argc; ++i) {
-    if (!strcmp(argv[i], "--base-path")) {
+    if (V_streq(argv[i], "--base-path")) {
       if (i + 1 < argc || strstr(argv[i + 1], "--") == argv[i + 1]) {
         strcpy_s(args.path_base, argv[i + 1]);
         ++i;
@@ -788,11 +789,11 @@ int main(int argc, char *argv[]) {
         PrintfUsage(argv[0]);
         return 1;
       }
-    } else if (!strcmp(argv[i], "--no-logo")) {
+    } else if (V_streq(argv[i], "--no-logo")) {
       show_logo = false;
-    } else if (!strcmp(argv[i], "--should-disassemble-shaders")) {
+    } else if (V_streq(argv[i], "--should-disassemble-shaders")) {
       args.should_disassemble_shaders = true;
-    } else if (!strcmp(argv[i], "--no-optimize-shaders")) {
+    } else if (V_streq(argv[i], "--no-optimize-shaders")) {
       args.should_optimize_shaders = false;
     } else {
       fprintf(stderr, "%s error: Unknown command line arg '%s'.", argv[0],

commit cb87ecbf34d17c5ce06b119bf6f0871c54006a43

remoteshadercompile: Fix build broken in 145ebd224f9c96c3c8c4cf2976f7ae1cf0bb9ac0

utils/remoteshadercompile/remoteshadercompile.cpp
@@ -20,7 +20,6 @@
 
 #include "windows/com_error_category.h"
 #include "tier0/dbg.h"
-#include "tier1/strtools.h"
 
 #include "tier0/memdbgon.h"
 
@@ -30,6 +29,10 @@ constexpr inline int kDefaultSendBufferSize{40000};
 constexpr inline int kDefaultReceiveBufferSize{40000};
 
 constexpr inline char kDefaultSharedPort[]{"20000"};
+
+[[nodiscard]] inline bool streq(IN_Z const char *l, IN_Z const char *r) {
+  return strcmp(l, r) == 0;
+}
 
 [[nodiscard]] std::error_code GetSystemError(int errc) noexcept {
   return std::error_code{errc, std::system_category()};
@@ -781,7 +784,7 @@ int main(int argc, char *argv[]) {
 
   bool show_logo{true};
   for (int i{1}; i < argc; ++i) {
-    if (V_streq(argv[i], "--base-path")) {
+    if (streq(argv[i], "--base-path")) {
       if (i + 1 < argc || strstr(argv[i + 1], "--") == argv[i + 1]) {
         strcpy_s(args.path_base, argv[i + 1]);
         ++i;
@@ -789,11 +792,11 @@ int main(int argc, char *argv[]) {
         PrintfUsage(argv[0]);
         return 1;
       }
-    } else if (V_streq(argv[i], "--no-logo")) {
+    } else if (streq(argv[i], "--no-logo")) {
       show_logo = false;
-    } else if (V_streq(argv[i], "--should-disassemble-shaders")) {
+    } else if (streq(argv[i], "--should-disassemble-shaders")) {
       args.should_disassemble_shaders = true;
-    } else if (V_streq(argv[i], "--no-optimize-shaders")) {
+    } else if (streq(argv[i], "--no-optimize-shaders")) {
       args.should_optimize_shaders = false;
     } else {
       fprintf(stderr, "%s error: Unknown command line arg '%s'.", argv[0],