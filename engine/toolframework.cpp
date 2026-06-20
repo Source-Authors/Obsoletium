Source-Authors/Obsoletium

commit c080b6d0f9e1b758805eeb37665bf82343287d4f

all: x86-64 binaries. TF2 backport

engine/toolframework.cpp
@@ -576,7 +576,7 @@ void CToolFrameworkInternal::LoadTools()
 {
 	m_bInToolMode = true;
 
-	// Load rootdir/bin/enginetools.txt
+	// Load rootdir/bin{/x64}/enginetools.txt
 	KeyValuesAD kv( "enginetools" );
 	Assert( kv );
 
@@ -591,7 +591,7 @@ void CToolFrameworkInternal::LoadTools()
 		{
 			if ( !Q_stricmp( tool->GetName(),  "library" ) )
 			{
-				// CHECK both bin/tools and gamedir/bin/tools
+				// CHECK both bin{/x64}/tools and gamedir/bin{/x64}/tools
 				LoadToolsFromLibrary( tool->GetString() );
 			}
 		}

public/filesystem_init.cpp
@@ -337,7 +337,8 @@ bool FileSystem_GetExecutableDir( OUT_Z_CAP(exeDirLen) char *exedir, unsigned ex
 		}
 		if ( pProject )
 		{
-			Q_snprintf( exedir, exeDirLen, "%s%c..%cbin", pProject, CORRECT_PATH_SEPARATOR, CORRECT_PATH_SEPARATOR );
+			// dimhotepus: x86-64 support.
+			Q_snprintf( exedir, exeDirLen, "%s%c..%c" PLATFORM_BIN_DIR, pProject, CORRECT_PATH_SEPARATOR, CORRECT_PATH_SEPARATOR );
 			return true;
 		}
 		return false;
@@ -352,22 +353,13 @@ bool FileSystem_GetExecutableDir( OUT_Z_CAP(exeDirLen) char *exedir, unsigned ex
 	// Return the bin directory as the executable dir if it's not in there
 	// because that's really where we're running from...
 	char ext[MAX_PATH];
-	V_StrRight( exedir, 4, ext );
-	if ( ext[0] != CORRECT_PATH_SEPARATOR ||
-#ifdef PLATFORM_64BITS
-		Q_stricmp( ext+1, "x64" ) != 0
-#else
-		Q_stricmp( ext+1, "bin" ) != 0
-#endif
-		)
+	// dimhotepus: x86-64 support. TF2 backport.
+	V_StrRight( exedir, ssize( PLATFORM_BIN_DIR ), ext);
+	if ( ext[0] != CORRECT_PATH_SEPARATOR || Q_stricmp( ext+1, PLATFORM_BIN_DIR ) != 0 )
 	{
 		Q_strncat( exedir, CORRECT_PATH_SEPARATOR_S, exeDirLen, COPY_ALL_CHARACTERS );
-		Q_strncat( exedir, "bin", exeDirLen, COPY_ALL_CHARACTERS );
-
-#ifdef PLATFORM_64BITS
-		Q_strncat( exedir, CORRECT_PATH_SEPARATOR_S "x64", exeDirLen, COPY_ALL_CHARACTERS );
-#endif
-
+		// dimhotepus: x86-64 support. TF2 backport.
+		Q_strncat( exedir, PLATFORM_BIN_DIR, exeDirLen, COPY_ALL_CHARACTERS );
 		Q_FixSlashes( exedir );
 	}
 	
@@ -375,17 +367,18 @@ bool FileSystem_GetExecutableDir( OUT_Z_CAP(exeDirLen) char *exedir, unsigned ex
 }
 
 template<size_t max_size>
-static bool FileSystem_GetBaseDir( char (&baseDir)[max_size] )
+static bool FileSystem_GetBaseDir( OUT_Z_ARRAY char (&baseDir)[max_size] )
 {
 	if ( FileSystem_GetExecutableDir( baseDir ) )
 	{
-#ifdef PLATFORM_64BITS
-		// dimhotepus: Need to strip x64, too.
 		V_StripFilename( baseDir );
-#endif
-		V_StripFilename( baseDir );
+		// dimhotepus: Need to strip PLATFORM_DIR, too.
+		if constexpr ( ssize( PLATFORM_DIR ) > 0 )
+			V_StripFilename( baseDir );
 		return true;
 	}
+
+	if ( max_size > 0 ) baseDir[max_size - 1] = '\0';
 	
 	return false;
 }
@@ -597,6 +590,8 @@ FSReturnCode_t FileSystem_LoadSearchPaths( CFSSearchPathsInit &initInfo )
 	bool bLowViolence = initInfo.m_bLowViolence;
 	for ( auto *pCur=pSearchPaths->GetFirstValue(); pCur; pCur=pCur->GetNextValue() )
 	{
+		// dimhotepus: x86-64 support. TF2 backport.
+		const char *pszPathID = pCur->GetName();
 		const char *pLocation = pCur->GetString();
 		const char *pszBaseDir = baseDir;
 
@@ -616,6 +611,14 @@ FSReturnCode_t FileSystem_LoadSearchPaths( CFSSearchPathsInit &initInfo )
 			// In the case of a game or a Steam-launched dedicated server, all the necessary prior engine content is mapped in with the Steam depots,
 			// so we can just use the path as-is.
 			pLocation += std::size( BASESOURCEPATHS_TOKEN ) - 1;
+		}
+
+		// dimhotepus: x86-64 support. TF2 backport.
+		char szBinLocation[MAX_PATH];
+		if ( Q_stricmp( pszPathID, "GAMEBIN" ) == 0 )
+		{
+			V_sprintf_safe( szBinLocation, "%s" PLATFORM_DIR, pLocation );
+			pLocation = szBinLocation;
 		}
 
 		CUtlStringList vecFullLocationPaths;
@@ -975,6 +978,14 @@ FSReturnCode_t FileSystem_SetBasePaths( IFileSystem *pFileSystem )
 		return SetupFileSystemError( false, FS_INVALID_PARAMETERS, "FileSystem_GetExecutableDir failed." );
 
 	pFileSystem->AddSearchPath( executablePath, "EXECUTABLE_PATH" );
+	// dimhotepus: x86-64 support.
+	if constexpr ( ssize( PLATFORM_DIR ) > 0 )
+	{
+		char baseBinFolder[MAX_PATH];
+		V_strcpy_safe( baseBinFolder, executablePath );
+		baseBinFolder[ V_strlen( baseBinFolder ) - ( ssize( PLATFORM_DIR ) - 1 ) ] = '\0';
+		pFileSystem->AddSearchPath( baseBinFolder, "EXECUTABLE_PATH" );
+	}
 
 	if ( !FileSystem_GetBaseDir( executablePath ) )
 		return SetupFileSystemError( false, FS_INVALID_PARAMETERS, "FileSystem_GetBaseDir failed." );

dedicated/vgui/vguihelpers.cpp
@@ -80,11 +80,7 @@ int StartVGUI(CreateInterfaceFn dedicatedFactory) {
   vgui::ivgui()->Start();
 
   // load the module
-#ifdef PLATFORM_64BITS
-  g_pFullFileSystem->GetLocalCopy("bin/x64/adminserver" DLL_EXT_STRING);
-#else
-  g_pFullFileSystem->GetLocalCopy("bin/adminserver" DLL_EXT_STRING);
-#endif
+  g_pFullFileSystem->GetLocalCopy(PLATFORM_BIN_DIR CORRECT_PATH_SEPARATOR_S "adminserver" DLL_EXT_STRING);
 
   g_hAdminServerModule = g_pFullFileSystem->LoadModule("adminserver");
   Assert(g_hAdminServerModule);

dedicated_main/main_posix.cpp
@@ -11,6 +11,7 @@
 #include <cerrno>
 
 #include "tier0/basetypes.h"
+#include "tier0/platform.h"
 #include "scoped_dll.h"
 
 namespace {
@@ -85,12 +86,7 @@ int main(int argc, char *argv[]) {
 
   char new_path_env[4096];
   snprintf(new_path_env, sizeof(new_path_env) - 1,
-#if !defined(PLATFORM_64BITS)
-           "LD_LIBRARY_PATH=%s/bin:%s",
-#else
-           "LD_LIBRARY_PATH=%s/bin/x64:%s",
-#endif
-           cwd, path_env);
+           "LD_LIBRARY_PATH=%s/" PLATFORM_BIN_DIR ":%s", cwd, path_env);
   if (putenv(new_path_env)) {
     const auto rc = errno;
     fprintf(stderr, "putenv (%s) failed: %s\n", new_path_env, strerror(rc));

dedicated_main/main_win.cpp
@@ -7,6 +7,8 @@
 
 #include <system_error>
 
+#include "tier0/platform.h"
+
 #include "scoped_dll.h"
 #include "winlite.h"
 
@@ -299,11 +301,7 @@ int APIENTRY WinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE old_instance,
   char base_directory_path[MAX_PATH], dedicated_dll_path[MAX_PATH];
   // Assemble the full path to our "dedicated.dll".
   _snprintf_s(dedicated_dll_path, _TRUNCATE,
-#if !defined(_WIN64)
-              "%s\\bin\\dedicated.dll",
-#else
-              "%s\\bin\\x64\\dedicated.dll",
-#endif
+              "%s\\" PLATFORM_BIN_DIR "\\dedicated.dll",
               GetBaseDirectory(module_name, base_directory_path));
 
   char user_error[1024];

engine/bugreporter.cpp
@@ -1910,11 +1910,7 @@ void CBugUIPanel::OnSubmit()
 	
 	if ( m_szSaveGameName[ 0 ] )
 	{
-#ifdef PLATFORM_64BITS
-		Msg( "save file save/x64/%s.sav\n", m_szSaveGameName );
-#else
-		Msg( "save file save/%s.sav\n", m_szSaveGameName );
-#endif
+		Msg( "save file save" PLATFORM_DIR CORRECT_PATH_SEPARATOR_S "%s.sav\n", m_szSaveGameName );
 	}
 	else
 	{
@@ -2054,11 +2050,7 @@ void CBugUIPanel::OnSubmit()
 		// Only attach .sav files in single player
 		if ( ( cl.m_nMaxClients == 1 ) && m_szSaveGameName[ 0 ] )
 		{
-#ifdef PLATFORM_64BITS
-			Q_snprintf( fn, sizeof( fn ), "save/x64/%s.sav", m_szSaveGameName );
-#else
-			Q_snprintf( fn, sizeof( fn ), "save/%s.sav", m_szSaveGameName );
-#endif
+			V_sprintf_safe( fn, "save" PLATFORM_DIR CORRECT_PATH_SEPARATOR_S "%s.sav", m_szSaveGameName );
 			Q_FixSlashes( fn );
 			attachedSave = AddFileToZip( fn );
 		}
@@ -2360,11 +2352,7 @@ CCallQueue g_UploadQueue;
 // 
 // 	if ( savefile && savefile[ 0 ] )
 // 	{
-// #ifdef PLATFORM_64BITS
-// 		Q_snprintf( localfile, sizeof( localfile ), "%s/save/x64/%s.sav", com_gamedir, savefile );
-// #else
-// 		Q_snprintf( localfile, sizeof( localfile ), "%s/save/%s.sav", com_gamedir, savefile );
-// #endif
+// 		Q_snprintf( localfile, sizeof( localfile ), "%s/save" PLATFORM_DIR CORRECT_PATH_SEPARATOR_S "%s.sav", com_gamedir, savefile );
 // 		Q_snprintf( remotefile, sizeof( remotefile ), "%s/%s.sav", GetSubmissionURL(bugId), savefile );
 // 		Q_FixSlashes( localfile );
 // 		Q_FixSlashes( remotefile );

engine/host_saverestore.cpp
@@ -500,11 +500,7 @@ char *CSaveRestore::GetSaveDir()
 	if ( szDirectory[0] ) return szDirectory;
 
 	// dimhotepus: Dropped / at the end to unify all places.
-#ifdef PLATFORM_64BITS
-	V_sprintf_safe(szDirectory, "save/x64", MOD_DIR);
-#else
-	V_sprintf_safe(szDirectory, "save", MOD_DIR);
-#endif
+	V_sprintf_safe(szDirectory, "save" PLATFORM_DIR, MOD_DIR);
 
 	return szDirectory;
 }

engine/pure_server.cpp
@@ -855,7 +855,8 @@ int CPureFileTracker::ListUserFiles( bool bListAll, const char *pchFilenameFind 
 		UserReportedFile_t &ufile = m_treeAllReportedFiles[file.m_idxFile];
 
 		bool bOutput = false;
-		if ( Q_stristr( ufile.m_filename.String(), "bin\\pak01" )!=NULL || Q_stristr( ufile.m_filename.String(), ".vpk" )!=NULL )
+		// dimhotepus: x86-64 support.
+		if ( Q_stristr( ufile.m_filename.String(), PLATFORM_BIN_DIR CORRECT_PATH_SEPARATOR_S "pak01" )!=NULL || Q_stristr( ufile.m_filename.String(), ".vpk" )!=NULL )
 			bOutput = true;
 		else
 		{

commit e658a57d9f0441a06a66de62923a35dbea81af63

all Fix x86 support broken in c080b6d0f9e1b758805eeb37665bf82343287d4f & 55074784ac11c64257ea1cd81358f6e79252933b

engine/toolframework.cpp
@@ -458,7 +458,7 @@ void CToolFrameworkInternal::LoadToolsFromLibrary( const char *dllname )
 {
 	char fixedDllName[ MAX_PATH ];
 	// dimhotepus: x86-64 support.
-	if ( ssize( PLATFORM_DIR ) > 0 && V_stristr( dllname, PLATFORM_DIR ) == nullptr )
+	if ( ssize( PLATFORM_DIR ) > 1 && V_stristr( dllname, PLATFORM_DIR ) == nullptr )
 	{
 		if ( V_strncmp( dllname, "tools", ssize( "tools" ) - 1 ) == 0 ) 
 		{

public/filesystem_init.cpp
@@ -383,7 +383,7 @@ static bool FileSystem_GetBaseDir( OUT_Z_ARRAY char (&baseDir)[max_size] )
 	{
 		V_StripFilename( baseDir );
 		// dimhotepus: Need to strip PLATFORM_DIR, too.
-		if constexpr ( ssize( PLATFORM_DIR ) > 0 )
+		if constexpr ( ssize( PLATFORM_DIR ) > 1 )
 			V_StripFilename( baseDir );
 		return true;
 	}
@@ -956,7 +956,7 @@ FSReturnCode_t FileSystem_SetBasePaths( IFileSystem *pFileSystem )
 
 	pFileSystem->AddSearchPath( executablePath, "EXECUTABLE_PATH" );
 	// dimhotepus: x86-64 support.
-	if constexpr ( ssize( PLATFORM_DIR ) > 0 )
+	if constexpr ( ssize( PLATFORM_DIR ) > 1 )
 	{
 		char baseBinFolder[MAX_PATH];
 		V_strcpy_safe( baseBinFolder, executablePath );

public/tier1/utlhashtable.h
@@ -83,9 +83,11 @@ public:
 	enum
 	{
 		INT16_STORAGE = ( sizeof( KVPair ) <= 2 ),
-		INT32_STORAGE = ( sizeof( KVPair ) > 2 && sizeof( KVPair ) <= 4 ),
 #ifdef PLATFORM_64BITS
+		INT32_STORAGE = ( sizeof( KVPair ) > 2 && sizeof( KVPair ) <= 4 ),
 		INT64_STORAGE = ( sizeof( KVPair ) > 4 ),
+#else
+		INT32_STORAGE = ( sizeof( KVPair ) > 2 ),
 #endif
 	};
 	using storage_t =