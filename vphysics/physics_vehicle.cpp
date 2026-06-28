Source-Authors/Obsoletium

commit f92320c018701f9cf4ac545fed0430860114147f

vphysics: Use safe memory clear

vphysics/physics_vehicle.cpp
@@ -1410,8 +1410,8 @@ void CVehicleController::WriteToTemplate( vphysics_save_cvehiclecontroller_t &co
 	controllerTemplate.m_bTraceData = m_bTraceData;
 	controllerTemplate.m_bOccupied = m_bOccupied;
 	controllerTemplate.m_bEngineDisable = m_bEngineDisable;
-	memcpy( &controllerTemplate.m_currentState, &m_currentState, sizeof(m_currentState) );
-	memcpy( &controllerTemplate.m_vehicleData, &m_vehicleData, sizeof(m_vehicleData) );
+	BitwiseClear( controllerTemplate.m_currentState );
+	BitwiseClear( controllerTemplate.m_vehicleData );
 	for (int i = 0; i < VEHICLE_MAX_WHEEL_COUNT; ++i )
 	{
 		controllerTemplate.m_pWheels[i] = m_pWheels[i];

commit e282cdfbf150be64154fe3d6e034225008f4d896

vphysics: Fix load of save in airboat broken in f92320c018701f9cf4ac545fed0430860114147f

vphysics/physics_vehicle.cpp
@@ -1410,8 +1410,8 @@ void CVehicleController::WriteToTemplate( vphysics_save_cvehiclecontroller_t &co
 	controllerTemplate.m_bTraceData = m_bTraceData;
 	controllerTemplate.m_bOccupied = m_bOccupied;
 	controllerTemplate.m_bEngineDisable = m_bEngineDisable;
-	BitwiseClear( controllerTemplate.m_currentState );
-	BitwiseClear( controllerTemplate.m_vehicleData );
+	BitwiseCopy( &m_currentState, &controllerTemplate.m_currentState, 1 );
+	BitwiseCopy( &m_vehicleData, &controllerTemplate.m_vehicleData, 1 );
 	for (int i = 0; i < VEHICLE_MAX_WHEEL_COUNT; ++i )
 	{
 		controllerTemplate.m_pWheels[i] = m_pWheels[i];