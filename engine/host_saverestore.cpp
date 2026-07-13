Source-Authors/Obsoletium

commit 43738e94dd7a11a5dd5ee49a4b8ad4fcafef50ed

[x64] Engine

engine/matsys_interface.cpp
@@ -67,7 +67,6 @@ int	d_lightstylenumframes[256];
 
 const MaterialSystem_Config_t *g_pMaterialSystemConfig;
 
-static CSysModule	*g_MaterialsDLL = NULL;
 bool g_LostVideoMemory = false;
 
 IMaterial*	g_materialEmpty;	// purple checkerboard for missing textures
@@ -146,24 +145,26 @@ ConVar mat_show_ab_hdr( "mat_show_ab_hdr", "0" );
 
 static void NukeModeSwitchSaveGames( void )
 {
-	if( g_pFileSystem->FileExists( "save\\modeswitchsave.sav" ) )
+	char modeswitch[MAX_OSPATH];
+	const char* saveDir = saverestore->GetSaveDir();
+
+	V_sprintf_safe( modeswitch, "%s/modeswitchsave.sav", saveDir );
+
+	if (g_pFileSystem->FileExists(modeswitch ))
 	{
-		g_pFileSystem->RemoveFile( "save\\modeswitchsave.sav" );
+		g_pFileSystem->RemoveFile( modeswitch );
 	}
-	if( g_pFileSystem->FileExists( "save\\modeswitchsave.tga" ) )
+
+	V_sprintf_safe( modeswitch, "%s/modeswitchsave.tga", saveDir );
+
+	if (g_pFileSystem->FileExists(modeswitch ))
 	{
-		g_pFileSystem->RemoveFile( "save\\modeswitchsave.tga" );
+		g_pFileSystem->RemoveFile( modeswitch );
 	}
 }
 
 void mat_hdr_level_Callback( IConVar *var, const char *pOldString, float flOldValue )
 {
-	if ( IsX360() )
-	{
-		// can't support, expected to be static
-		return;
-	}
-
 #ifdef CSS_PERF_TEST
 	ConVarRef hdr( var );
 	if ( hdr.GetInt() > 0 )
@@ -232,9 +233,7 @@ static const char *s_pRegistryConVars[] =
 	"mat_reducefillrate",
 	"r_shadowrendertotexture",
 	"r_rootlod",
-#ifndef _X360
 	"r_waterforceexpensive",
-#endif
 	"r_waterforcereflectentities",
 	"mat_antialias",
 	"mat_aaquality",
@@ -243,8 +242,8 @@ static const char *s_pRegistryConVars[] =
 	"mat_hdr_level",
 	"mat_colorcorrection",
 
+	// dimhotepus: Dropped empty string.
 	// NOTE: Empty string must always be last!
-	""
 };
 
 #if defined( OSX )
@@ -438,7 +437,8 @@ static void ReadMaterialSystemConfigFromRegistry( MaterialSystem_Config_t &confi
 	const char *szMonitorGamma = ReadVideoConfigString( "ScreenMonitorGamma", "2.2" );
 	if ( szMonitorGamma && szMonitorGamma[0] )
 	{
-		float flMonitorGamma = atof( szMonitorGamma );
+		// dimhotepus: atof -> strtof
+		float flMonitorGamma = strtof( szMonitorGamma, nullptr );
 		// temp, to make sure people with gamma values saved in the old format don't max out
 		if (flMonitorGamma > 3.0f)
 		{
@@ -451,15 +451,15 @@ static void ReadMaterialSystemConfigFromRegistry( MaterialSystem_Config_t &confi
 		config.m_fMonitorGamma = mat_monitorgamma.GetFloat();
 	}
 
-	for ( int i = 0; s_pRegistryConVars[i][0]; ++i )
+	for ( auto *var : s_pRegistryConVars )
 	{
-		int nValue = ReadVideoConfigInt( s_pRegistryConVars[i], 0x80000000 );
-		if ( nValue == 0x80000000 )
+		int nValue = ReadVideoConfigInt( var, -1 );
+		if ( nValue == -1 )
 			continue;
 		
-		nValue = OverrideVideoConfigFromCommandLine( s_pRegistryConVars[i], nValue );
+		nValue = OverrideVideoConfigFromCommandLine( var, nValue );
 
-		ConVarRef conVar( s_pRegistryConVars[i] );
+		ConVarRef conVar( var );
 		if ( conVar.IsValid() )
 		{
 			conVar.SetValue( nValue );
@@ -549,13 +549,13 @@ static void WriteMaterialSystemConfigToRegistry( const MaterialSystem_Config_t &
 	// Registry only stores ints, so divide/multiply by 100 when reading/writing.
 	WriteVideoConfigString( "ScreenMonitorGamma", mat_monitorgamma.GetString() );
 
-	for ( int i = 0; s_pRegistryConVars[i][0]; ++i )
+	for ( auto *var : s_pRegistryConVars )
 	{
-		ConVarRef conVar( s_pRegistryConVars[i] );
+		ConVarRef conVar( var );
 		if ( !conVar.IsValid() )
 			continue;
 
-		WriteVideoConfigInt( s_pRegistryConVars[i], conVar.GetInt() );
+		WriteVideoConfigInt( var, conVar.GetInt() );
 	}
 #endif
 }
@@ -1160,8 +1160,7 @@ void InitWellKnownRenderTargets( void )
 	// Begin block in which all render targets should be allocated
 	materials->BeginRenderTargetAllocation();
 
-	// JasonM - 
-	// Do we put logic in here to determine which of these to create, based upon DX level, HDR enable etc?
+	// TODO: JasonM - Do we put logic in here to determine which of these to create, based upon DX level, HDR enable etc?
 	// YES! DX Level should gate these
 
 	// before we create anything, see if VR mode wants to override the "framebuffer" size
@@ -2003,7 +2002,7 @@ int FindOrAddMesh( IMaterial *pMaterial, int vertexCount )
 		return i;
 	}
 
-	int index = g_Meshes.AddToTail();
+	intp index = g_Meshes.AddToTail();
 	g_Meshes[index].vertCount = vertexCount;
 	g_Meshes[index].vertexFormat = format;
 	g_Meshes[index].pMaterial = pMaterial;

engine/saverestore_filesystem.h
@@ -37,7 +37,7 @@ public:
 	virtual FSAsyncStatus_t	AsyncAppend( const char *pFileName, const void *pSrc, int nSrcBytes, bool bFreeMemory, FSAsyncControl_t *pControl = NULL ) = 0;
 	virtual FSAsyncStatus_t	AsyncAppendFile( const char *pDestFileName, const char *pSrcFileName, FSAsyncControl_t *pControl = NULL ) = 0;
 
-	virtual void			DirectoryCopy( const char *pPath, const char *pDestFileName, bool bIsXSave ) = 0;
+	virtual void			DirectoryCopy( const char *pPath, const char *pDestFileName ) = 0;
 	virtual	bool			DirectoryExtract( FileHandle_t pFile, int fileCount, bool bIsXSave ) = 0;
 	virtual int				DirectoryCount( const char *pPath ) = 0;
 	virtual void			DirectoryClear( const char *pPath, bool bIsXSave ) = 0;

commit 2d763382511b1255b917b3e1c2d6c861baca2571

engine: Fix clear save dir from old saves, do mode switch saves (broken in 43738e94dd7a11a5dd5ee49a4b8ad4fcafef50ed)

engine/host_saverestore.cpp
@@ -503,7 +503,7 @@ char *CSaveRestore::GetSaveDir()
 	if ( szDirectory[0] ) return szDirectory;
 
 	// dimhotepus: Dropped / at the end to unify all places.
-	V_sprintf_safe(szDirectory, "save" PLATFORM_DIR, MOD_DIR);
+	V_strcpy_safe(szDirectory, "save" PLATFORM_DIR);
 
 	return szDirectory;
 }
@@ -530,7 +530,6 @@ void CSaveRestore::AgeSaveFile( const char *pName, const char *ext, int count )
 {
 	char newName[MAX_OSPATH], oldName[MAX_OSPATH];
 
-	// dimhotepus: Use MOD inside GetSaveDir.
 	if ( count == 1 )
 	{
 		V_sprintf_safe( oldName, "%s/%s.%s", GetSaveDir(), pName, ext );	// quick.sav.
@@ -540,9 +539,9 @@ void CSaveRestore::AgeSaveFile( const char *pName, const char *ext, int count )
 		V_sprintf_safe( oldName, "%s/%s%02d.%s", GetSaveDir(), pName, count-1, ext );	// quick04.sav, etc.
 	}
 	
-	// dimhotepus: Use MOD inside GetSaveDir.
 	V_sprintf_safe( newName, "%s/%s%02d.%s", GetSaveDir(), pName, count, ext );
-
+	
+	// dimhotepus: Use MOD for fs.
 	// Scroll the name list down (rename quick04.sav to quick05.sav)
 	if ( g_pFileSystem->FileExists( oldName, MOD_DIR ) )
 	{
@@ -1022,8 +1021,6 @@ bool CSaveRestore::LoadGame( const char *pName )
 
 	m_bClearSaveDir = false;
 	DoClearSaveDir();
-
-	bool bLoadedToMemory = false;
 	
 	int iElapsedMinutes = 0;
 	int iElapsedSeconds = 0;
@@ -1055,10 +1052,6 @@ bool CSaveRestore::LoadGame( const char *pName )
 		}
 		else
 		{
-			if ( bLoadedToMemory )
-			{
-				g_pSaveRestoreFileSystem->RemoveFile( name );
-			}
 			return NULL;
 		}
 
@@ -1075,11 +1068,6 @@ bool CSaveRestore::LoadGame( const char *pName )
 			// dimhotepus: Msg -> Warning.
 			Warning( "Map '%s' missing or invalid\n", gameHeader.mapName );
 			validload = false;
-		}
-		
-		if ( bLoadedToMemory )
-		{
-			g_pSaveRestoreFileSystem->RemoveFile( name );
 		}
 	}
 	else
@@ -1827,7 +1815,9 @@ void CSaveRestore::RestoreClientState( char const *fileName, bool adjacent )
 void CSaveRestore::RestoreAdjacenClientState( char const *map )
 {
 	char name[256];
-	Q_snprintf( name, sizeof( name ), "%s/%s.HL2", GetSaveDir(), GetSaveGameMapName( map ) );// DON'T FixSlashes on this, it needs to be //MOD
+	Q_snprintf( name, sizeof( name ), "%s/%s.HL2", GetSaveDir(), GetSaveGameMapName( map ) );
+	// dimhotepus: Fix file name.
+	V_FixSlashes( name );
 	COM_CreatePath( name );
 
 	RestoreClientState( name, true );
@@ -2579,7 +2569,7 @@ void CSaveRestore::DirectoryCount( const char *pPath, int *pResult )
 //-----------------------------------------------------------------------------
 void CSaveRestore::DirectoryClear( const char *pPath )
 {
-	g_pSaveRestoreFileSystem->DirectoryClear( pPath, false );
+	g_pSaveRestoreFileSystem->DirectoryClear( pPath );
 }
 
 
@@ -2603,6 +2593,8 @@ void CSaveRestore::DoClearSaveDir()
 	char szName[MAX_OSPATH];
 
 	V_strcpy_safe( szName, GetSaveDir() );
+	// dimhotepus: GetSaveDir after changes missed separator, readd it.
+	V_strcat_safe( szName, CORRECT_PATH_SEPARATOR_S );
 	Q_FixSlashes( szName );
 	// Create save directory if it doesn't exist
 	Sys_mkdir( szName );

engine/matsys_interface.cpp
@@ -139,16 +139,16 @@ static void NukeModeSwitchSaveGames( void )
 
 	V_sprintf_safe( modeswitch, "%s/modeswitchsave.sav", saveDir );
 
-	if (g_pFileSystem->FileExists(modeswitch ))
+	if (g_pFileSystem->FileExists(modeswitch, "MOD" ))
 	{
-		g_pFileSystem->RemoveFile( modeswitch );
+		g_pFileSystem->RemoveFile( modeswitch, "MOD" );
 	}
 
 	V_sprintf_safe( modeswitch, "%s/modeswitchsave.tga", saveDir );
 
-	if (g_pFileSystem->FileExists(modeswitch ))
+	if (g_pFileSystem->FileExists(modeswitch, "MOD" ))
 	{
-		g_pFileSystem->RemoveFile( modeswitch );
+		g_pFileSystem->RemoveFile( modeswitch, "MOD" );
 	}
 }
 

engine/saverestore_filesystem.cpp
@@ -209,7 +209,8 @@ public:
 			Q_snprintf( szName, sizeof( szName ), "%s/%s", saverestore->GetSaveDir(), list[i].szFileName );
 			Q_FixSlashes( szName );
 
-			unsigned fileSize = g_pFileSystem->Size( szName );
+			// dimhotepus: Correctly check file size.
+			unsigned fileSize = g_pFileSystem->Size( szName, "MOD" );
 			if ( fileSize )
 			{
 				Assert( sizeof(list[i].szFileName) == MAX_PATH );
@@ -282,7 +283,7 @@ public:
 	//-----------------------------------------------------------------------------
 	// Purpose: Clears the save directory of all temporary files (*.hl)
 	//-----------------------------------------------------------------------------
-	void DirectoryClear( const char *pPath, bool bIsXSave )
+	void DirectoryClear( const char *pPath )
 	{
 		char		szPath[ MAX_PATH ];
 		
@@ -290,14 +291,7 @@ public:
 		RunCodeAtScopeExit(Sys_FindClose());
 		while ( findfn != NULL )
 		{
-			if ( !bIsXSave )
-			{
-				Q_snprintf( szPath, sizeof( szPath ), "%s/%s", saverestore->GetSaveDir(), findfn );
-			}
-			else
-			{
-				Q_snprintf( szPath, sizeof( szPath ), "%s:\\%s", GetCurrentMod(), findfn );
-			}
+			Q_snprintf( szPath, sizeof( szPath ), "%s/%s", saverestore->GetSaveDir(), findfn );
 
 			// Delete the temporary save file
 			g_pFileSystem->RemoveFile( szPath, "MOD" );

engine/saverestore_filesystem.h
@@ -52,7 +52,7 @@ public:
 	virtual void			DirectoryCopy( const char *pPath, const char *pDestFileName ) = 0;
 	virtual	bool			DirectoryExtract( FileHandle_t pFile, int fileCount ) = 0;
 	virtual int				DirectoryCount( const char *pPath ) = 0;
-	virtual void			DirectoryClear( const char *pPath, bool bIsXSave ) = 0;
+	virtual void			DirectoryClear( const char *pPath ) = 0;
 
 	virtual void			AuditFiles( void ) = 0;
 	virtual bool			LoadFileFromDisk( const char *pFilename ) = 0;